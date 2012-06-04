4e8fa4e1df9404023452a66bf080b8c301c41290
Andi Bräu <ab@andi95.de>
2012-06-05 01:05:15 +0200

    added some lawful things

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -23,21 +23,23 @@
             <ul class="nav">
               <li <?php echo getActiveClass("index.php")?> ><a href="./index.php">Home</a></li>
               <li class="dropdown">
-						<a href="#"	class="dropdown-toggle"	data-toggle="dropdown">Informationen <b class="caret"></b></a>
+						<a href="wiki.php" data-target="#"	class="dropdown-toggle"	data-toggle="dropdown">Informationen <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-				  			<li<?php echo getActiveClass("wiki.php")?>><a href="./wiki.php">Wiki</a></li>
-							<li<?php echo getActiveClass("news.php")?>><a href="./news.php" >Newsserver</a></li>
+				  			<li <?php echo getActiveClass("wiki.php")?>><a href="./wiki.php">Wiki</a></li>
+							<li <?php echo getActiveClass("news.php")?>><a href="./news.php" >Newsserver</a></li>
+							<li <?php echo getActiveClass("termsofuse.php")?>><a href="./termsofuse.php" >Nutzungsbedingungen</a></li>
+							<li <?php echo getActiveClass("picopeering.php")?>><a href="./picopeering.php" >Picopeering Aggreement</a></li>
 						</ul>
 					</li>
 					<li class="dropdown">
-						<a href="#"	class="dropdown-toggle"	data-toggle="dropdown">Karten <b class="caret"></b></a>
+						<a href="map.php" data-target="#"	class="dropdown-toggle"	data-toggle="dropdown">Karten <b class="caret"></b></a>
 						<ul class="dropdown-menu">
 							<li <?php echo getActiveClass("map.php")?> ><a href="./map.php">Geographische Karte</a></li>
 							<li <?php echo getActiveClass("topology.php")?> ><a href="./topology.php">Topologische Karte</a></li>
 						</ul>					
 					</li>
 				   <li class="dropdown">
-				   	<a href="#"	class="dropdown-toggle"	data-toggle="dropdown">Status <b class="caret"></b></a>
+				   	<a href="status.php"	data-target="#" class="dropdown-toggle"	data-toggle="dropdown">Status <b class="caret"></b></a>
 				   	<ul class="dropdown-menu">
 				  			<li <?php echo getActiveClass("status.php")?> ><a href="./status.php" >OLSR Status</a></li> 	
 				  			<li <?php echo getActiveClass("monitoring.php")?> ><a href="./monitoring.php" >Monitoring</a></li>
@@ -46,11 +48,22 @@
               <li class="divider-vertical"></li>
                <ul class="nav pull-right">         
               	<li <?php echo getActiveClass("about.php")?> ><a href="./about.php">Über</a></li>
+              	</ul>
+              	<li class="dropdown">
+				   	<a href="impressum.php" data-target="#" class="dropdown-toggle"	data-toggle="dropdown">Impressum <b class="caret"></b></a>
+				   	<ul class="dropdown-menu">
+				   	<li <?php echo getActiveClass("impressum.php")?> ><a href="./impressum.php">Impressum</a></li>
               	<li <?php echo getActiveClass("contact.php")?> ><a href="./contact.php">Kontakt</a></li>
-              </ul>
+              	</ul>
               </ul>
             
           </div>
         </div>
       </div>
     </div>
+    
+    <noscript><div class="container">
+    <div class="alert alert-error">
+    <strong>Achtung!</strong> Bitte aktiviere Javascript in Deinem Browser, um diese Website vollständig nutzen zu können.
+    </div>
+    </div></noscript>
---
+++ termsofuse.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Weimarnetz - Über</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Unknown" >
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="apple-touch-icon" href="http://twitter.github.com/bootstrap/examples/images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="http://twitter.github.com/bootstrap/examples/images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="http://twitter.github.com/bootstrap/examples/images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+<script type="text/javascript">
+    
+    function getElement(aID)
+    {
+        return (document.getElementById) ?
+            document.getElementById(aID) :document.all[aID];
+    }
+
+    function getIFrameDocument(aID){ 
+        var rv = null; 
+        var frame=getElement(aID);
+        // if contentDocument exists, W3C compliant (e.g. Mozilla) 
+        if (frame.contentDocument)
+            rv = frame.contentDocument;
+        else // bad Internet Explorer  ;)
+            rv = document.frames[aID].document;
+        return rv;
+    }
+
+    function adjustMyFrameHeight()
+    {
+        var frame = getElement("myFrame");
+        var frameDoc = getIFrameDocument("myFrame");
+        frame.height = frameDoc.body.offsetHeight;
+    }
+</script>
+
+	
+<?php include("./inc/header.inc.php")?>
+
+	 <div class="container">
+<h1>Nutzungsbedingungen</h1>
+
+<p>Dieses sind die Nutzungsbedingungen f&uuml;r das Netzwerk Freifunk Weimar
+(weimarnetz).
+Durch die Verbindung zum Netzwerk oder dessen Nutzung akzepiert der Client
+(Benutzer) diese Bedingungen.
+</p>
+<h2> Anwendungsbereich </h2>
+<p>Das offene und freie drahtlose Netzwerk Freifunk Weimar wird von
+Freiwilligen betrieben ("Betreiber"), die die n&ouml;tigen Ger&auml;te und
+Internet-Anschl&uuml;sse ("Infrastruktur") auf eigene Kosten zur Verf&uuml;gung stellen.
+Diese Nutzungsbestimmungen regeln die Inanspruchnahme des Netzwerks Freifunk
+Weimar durch die Teilnehmer, welche sich mittels Computer, Handheld a.&auml;
+("Endger&auml;t") mit dem Netzwerk Freifunk Weimar verbinden.
+</p><p>Der Zugang zum Netzwerk Freifunk Weimar wird nicht garantiert.
+Er kann jederzeit und ohne Angabe von Gr&uuml;nden unterbrochen oder f&uuml;r bestimmte
+Endger&auml;te und Teilnehmer gesperrt werden.
+</p>
+<h2> Gesetzlich verbotene Handlungen </h2>
+<p>Der Teilnehmer verpflichtet sich, keine Handlungen vorzunehmen bzw.
+Handlungen zu unterlassen, welche gesetztliche Bestimmungen oder die Rechte
+Dritter verletzen.
+</p>
+<h2> Gesetzlich verbotene Inhalte</h2>
+<p>Der Teilnehmer verpflichtet sich, keine Inhalte &uuml;ber das Netzwerk Freifunk
+Weimar zu &uuml;bertragen, welche gegen geltendes Recht verstossen.
+</p>
+<h2> Fair Use Policy</h2>
+<p>Der Teilnehmer verpflichtet sich, das Netzwerk Freifunk Weimar nicht in
+einer Weise zu beanspruchen, welche die Infrastruktur, das Netzwerk Freifunk
+Weimar selbst, dessen Betreiber oder andere Teilnehmer beintr&auml;chtigt.
+</p>
+<h2> Sicherheit</h2>
+<p>Das Netzwerk Freifunk Weimar ist, wie das Internet, unverschl&uuml;sselt und
+offen. Jeder Teilnehmer ist selbst f&uuml;r die Sicherheit seiner Verbindungen und
+seiner Endger&auml;te verantwortlich.
+</p>
+<h2> Haftungsauschluss </h2>
+<p>Die Betreiber lehnen jede Haftung f&uuml;r Datenverlust, unbefugter Zugriff auf
+Endger&auml;te, Sch&auml;den an Endger&auml;ten oder finanzielle Verluste, die ein Teilnehmer
+durch die Nutzung des Netzwerks Freifunk Weimar erleidet, ab.
+</p>
+
+Diese Bedingungen haben sind denen von <a href="http://wiki.freifunk-hannover.de/Willkommen" target="_blank">Freifunk Hannover</a> angelehnt.<br>
+Weiterhin gilt das <a href="picopeering.php">Picopeering Aggrement</a>.
+
+	</div>
+
+    	
+<?php include("./inc/footer.inc.php")?>
+
+
+</body></html>
