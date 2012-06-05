6a96418c10b38e0d0515ae81c255a1b522cbb9a7
Andi Bräu <ab@andi95.de>
2012-06-05 23:10:44 +0200

    resolved some errors

--- contact.php
+++ contact.php
@@ -28,7 +28,7 @@
     <link rel="apple-touch-icon" sizes="114x114" href="http://twitter.github.com/bootstrap/examples/images/apple-touch-icon-114x114.png">
   </head>
 
-  <body>
+  <body data-spy="scroll" data-target=".subnav" data-offset="50">
 <script type="text/javascript">
     
     function getElement(aID)
@@ -75,12 +75,14 @@
 		<p>Wir treffen uns wöchentlich jeden Dienstag ab 20 Uhr, meist im Maschinenraum in der Marienstraße 18. Neben Vorträgen und der Möglichkeit zum Erfahrungsaustausch gibt es viel praktische Firmwareentwicklung und wenn nötig, Bastelorgien am Gerät. Falls dann noch Zeit bleibt, kann man Mate und Bier genießen.<br> Wir begrüßen außerdem gern neue Interessenten und geben Hilfestellung bei Problemen oder der Inbetriebnahme eines neuen Knoten.</p>
 		<p>Wann und wo das nächste Treffen stattfindet und ob es aktuelle Themen zu bearbeiten gibt, steht im <a href="http://wireless.subsignal.org/index.php?title=Treffen" target="_blank" >Wiki</a>.</p>
 		</div>
+		</div>
+		<div class="row">
 		<div class="span6"><h2>Postalisch</h2>
 	 	<address><strong>Weimarnetz e.V.</strong><br>
 	 	Marienstr. 18<br>
 	 	99423 Weimar</address>
 	 	</div>
-	 	<div clas="span6"><h2>Telefonisch</h2>
+	 	<div class="span6"><h2>Telefonisch</h2>
 		<address><strong>Weimarnetz Sorgentelefon</strong><br>
 		03643 / 544 304</address>	 	
 	 	</div> 
--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,3 +1,10 @@
+<br />
+<br />
+<div class="container">
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="img/cc.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Homepage Weimarnetz e.V.</span> von <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Weimarnetz e.V.</span> steht unter einer <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/">Creative Commons Namensnennung-Nicht-kommerziell-Weitergabe unter gleichen Bedingungen 3.0 Deutschland Lizenz</a>.<br />Beruht auf einem Inhalt unter <a xmlns:dct="http://purl.org/dc/terms/" href="http://www.weimarnetz.de" rel="dct:source">www.weimarnetz.de</a>
+</div>
+
+
 <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
