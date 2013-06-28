70af2010668a9d5b15dcf03e779532ce86ee1108
Andi Bräu <ab@andi95.de>
2013-06-28 14:48:58 +0200

    relative path correction in menus

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -21,42 +21,51 @@
           <a class="brand" href="./index.php">Weimarnetz Rootserver</a>
           <div class="nav-collapse">
             <ul class="nav">
-              <li <?php echo getActiveClass("index.php")?> ><a href="./index.php">Home</a></li>
+              <li <?php echo getActiveClass("index.php")?> ><a href="/index.php">Home</a></li>
               <li class="dropdown">
-						<a href="wiki.php" data-target="#"	class="dropdown-toggle"	data-toggle="dropdown">Informationen <b class="caret"></b></a>
+						<a href="/wiki.php" data-target="#"	class="dropdown-toggle"	data-toggle="dropdown">Informationen <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-				  			<li <?php echo getActiveClass("wiki.php")?>><a href="./wiki.php">Wiki</a></li>
-							<li <?php echo getActiveClass("news.php")?>><a href="./news.php" >Newsserver</a></li>
-							<li <?php echo getActiveClass("termsofuse.php")?>><a href="./termsofuse.php" >Nutzungsbedingungen</a></li>
-							<li <?php echo getActiveClass("picopeering.php")?>><a href="./picopeering.php" >Picopeering Aggreement</a></li>
+				  			<li <?php echo getActiveClass("wiki.php")?>><a href="/wiki.php">Wiki</a></li>
+							<li <?php echo getActiveClass("news.php")?>><a href="/news.php" >Newsserver</a></li>
+							<li <?php echo getActiveClass("termsofuse.php")?>><a href="/termsofuse.php" >Nutzungsbedingungen</a></li>
+							<li <?php echo getActiveClass("picopeering.php")?>><a href="/picopeering.php" >Picopeering Aggreement</a></li>
 						</ul>
 					</li>
 					<li class="dropdown">
-						<a href="map.php" data-target="#"	class="dropdown-toggle"	data-toggle="dropdown">Karten <b class="caret"></b></a>
+						<a href="/map.php" data-target="#"	class="dropdown-toggle"	data-toggle="dropdown">Karten <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li <?php echo getActiveClass("map.php")?> ><a href="./map.php">Geographische Karte</a></li>
-							<li <?php echo getActiveClass("topology.php")?> ><a href="./topology.php">Topologische Karte</a></li>
+							<li <?php echo getActiveClass("map.php")?> ><a href="/map.php">Geographische Karte</a></li>
+							<li <?php echo getActiveClass("topology.php")?> ><a href="/topology.php">Topologische Karte</a></li>
 						</ul>					
 					</li>
 				   <li class="dropdown">
-				   	<a href="status.php"	data-target="#" class="dropdown-toggle"	data-toggle="dropdown">Status <b class="caret"></b></a>
+				   	<a href="/status.php"	data-target="#" class="dropdown-toggle"	data-toggle="dropdown">Status <b class="caret"></b></a>
 				   	<ul class="dropdown-menu">
-				  			<li <?php echo getActiveClass("status.php")?> ><a href="./status.php" >OLSR Status</a></li> 	
-				  			<li <?php echo getActiveClass("monitoring.php")?> ><a href="./monitoring.php" >Monitoring</a></li>
+				  			<li <?php echo getActiveClass("status.php")?> ><a href="/status.php" >OLSR Status</a></li> 	
+				  			<li <?php echo getActiveClass("monitoring.php")?> ><a href="/monitoring.php" >Monitoring</a></li>
 				  			</ul>
 				   </li>
+				   <li class="dropdown">
+				   	<a href="/freifunk/firmware/ar71xx"	data-target="#" class="dropdown-toggle"	data-toggle="dropdown">Firmware <b class="caret"></b></a>
+				   	<ul class="dropdown-menu">
+				  			<li <?php echo (strpos(dirname($_SERVER["SCRIPT_NAME"]), "/freifunk/firmware/ar71xx") !== false) ? "class='active'":"" ;?> ><a href="/freifunk/firmware/ar71xx" >Atheros (TP-Link, Ubiqity, usw)</a></li> 	
+				  			<li <?php echo getActiveClass("freifunk/firmware/brcm47xx")?> ><a href="/freifunk/firmware/brcm47xx" >Broadcom (Linksys, Buffalo, usw.)</a></li>
+				  			<li <?php echo getActiveClass("freifunk/firmware/nightlies")?> ><a href="/freifunk/firmware/nightlies" >Nightly Builds</a></li>
+				  			</ul>
+				</li>
+
 				   			<li class="divider-vertical"></li>
                <ul class="nav pull-left">         
-              	<li <?php echo getActiveClass("partner.php")?> ><a href="./partner.php">Partner</a></li>
+              	<li <?php echo getActiveClass("partner.php")?> ><a href="/partner.php">Partner</a></li>
               	</ul>
               <ul class="nav pull-right">         
-              	<li <?php echo getActiveClass("about.php")?> ><a href="./about.php">Über</a></li>
+              	<li <?php echo getActiveClass("about.php")?> ><a href="/about.php">Über</a></li>
               	</ul>
               	<li class="dropdown">
-				   	<a href="impressum.php" data-target="#" class="dropdown-toggle"	data-toggle="dropdown">Impressum <b class="caret"></b></a>
+				   	<a href="/impressum.php" data-target="#" class="dropdown-toggle"	data-toggle="dropdown">Impressum <b class="caret"></b></a>
 				   	<ul class="dropdown-menu">
-				   	<li <?php echo getActiveClass("impressum.php")?> ><a href="./impressum.php">Impressum</a></li>
-              	<li <?php echo getActiveClass("contact.php")?> ><a href="./contact.php">Kontakt</a></li>
+				   	<li <?php echo getActiveClass("impressum.php")?> ><a href="/impressum.php">Impressum</a></li>
+              	<li <?php echo getActiveClass("contact.php")?> ><a href="/contact.php">Kontakt</a></li>
               	</ul>
               </ul>
             
