163309b39440e66df4b347fd0a836ece5790e1e1
Max F. Albrecht <1@178.is>
2013-08-22 01:27:36 +0200

    [refactor] redo 'Partner' showcase

--- partner.php
+++ partner.php
@@ -8,52 +8,69 @@
 
   <?php include("./inc/navbar.inc.php") ?>
 
-  <div class="container">
+  <div class="showcase container">
 
 	 <h1>Unsere Partner und Sponsoren</h1>
-	 Wir bedanken uns bei all unseren Partnern, Sponsoren und Förderern für die uns entgegengebrachte Unterstützung.
+	 <p class="lead">Wir bedanken uns bei all unseren Partnern, Sponsoren und Förderern für die uns entgegengebrachte Unterstützung.</p>
+
 	 <div class="row">
-	 <div class="span12">&nbsp;</div>
-	 <div class="span12">
-	 <div class="row">
-	 <div class="span6">
-	 					<a href="http://www.openwrt.org" target="_window"><img src="img/Banner/openwrt-logo.png" width="240" height="61" alt="OpenWrt" longdesc="OpenWrt schafft mit seiner Firmware für viele verschiedene WLAN-Router die Grundlagen für ein funktionierendes Netz" /></a><br />
-	 					OpenWrt ermöglich uns mit seiner Firmware, unsere Router für unseren Einsatzzweck perfekt anzupassen. Durch die stetige Weiterentwicklung der OpenWrt-Community sind wir bestens für die Zukunft gerüstet.
-			<br /><br />
-						<a href="http://www.mindfactory.de" target="_window"><img src="img/Banner/mf_blue_on_white_logo.png" width="240" height="70" alt="Mindfactory.de" /></a><br />
-						Mindfactory ist der Onlineshop für PC Hardware, Software, Freifunkrouter und mehr. Von Mindfactory erhalten wir Unterstützung beim Kauf von Hardware und Komponenten.<br /><br />
-			</div>
-			<div class="span4 offset2">
-			<a href="http://www.sublab.org" target="_window"><img src="img/Banner/banner_sublab.svg" width="177" height="213" alt="Sublab" /></a><br />
-			Das Sublab ist eine Freifunkkeimzelle in Leipzig und neue Heimat unserer Gründerväter<br /><br />
-			</div>
-		</div>
-		</div>
-		</div>
-		<div class="row">
-		<div class="span6 offset3">
-			<a href="http://www.maschinenraum.tk" target="_window"><img src="img/Banner/maschinenraum.png" width="640" height="96" alt="Maschinenraum" /></a><br />
-			Der Maschinenraum ist unsere Basis und bietet uns Platz für Testorgien und unsere wöchentlichen Treffen.<br /><br />
-		</div>
-		</div>
-		<div class="row">
-		<div class="span3 offset1">
-		<a href="http://www.bittorf-wireless.de" target="_window"><img src="img/Banner/banner_bittorfwireless.png" width="306" height="38" alt="Bittorf Wireless" /></a><br />
-		Bittorf Wireless unterstützt uns mit Technik, Wissen und Softwareentwicklung.<br /><br />
-		</div>
-		</div>
-		<div class="row">
-		<div class="span3 offset4">
-		<a href="http://www.cstorch.de" target="_window"><img src="img/Banner/cstorch_banner.jpg" width="468" height="60" alt="christian storch - Informationstechnologie" /></a><br />
-		Christian Storch Informationstechnologie hilft uns bei Softwareentwicklung, Support und Installationen.<br /><br />		
-		</div>		
-		</div>
-		<div class="row">
-		<div class="span3 offset7">
-		<a href="http://www.weimar.de" target="_window"><img src="img/Banner/weimar_logo_print.png" width="143" height="58" alt="Weimar. Kulturstadt Europas" /></a><br />
-		Wir arbeiten mit der Stadt Weimar zusammen und finden gemeinsam neue Standorte, die mit Weimarnetz versorgt werden sollen.		
-		</div>
-		</div>
+     
+     <style>
+     .showcase {
+       text-align: center;       
+     }
+     .container div {
+       margin-bottom: 3em;
+     }
+     .container a.thumbnail {
+       margin-bottom: 1em;
+     }
+     .container a.thumbnail img {
+       width: 80%;
+       margin: 0.25em auto;
+     }
+     </style>
+     
+  	 <div class="col-sm-4 col-lg-3">
+       <a class="thumbnail" href="http://www.openwrt.org" target="_window"><img src="img/Banner/openwrt-logo.png" alt="OpenWrt" longdesc="OpenWrt schafft mit seiner Firmware für viele verschiedene WLAN-Router die Grundlagen für ein funktionierendes Netz" /></a>
+       <p>OpenWrt ermöglich uns mit seiner Firmware, unsere Router für unseren Einsatzzweck perfekt anzupassen. Durch die stetige Weiterentwicklung der OpenWrt-Community sind wir bestens für die Zukunft gerüstet.</p>
+     </div>
+      
+     <div class="col-sm-4 col-lg-3">
+       <a class="thumbnail" href="http://www.mindfactory.de" target="_window"><img src="img/Banner/mf_blue_on_white_logo.png" alt="Mindfactory.de" /></a>
+       <p>Mindfactory ist der Onlineshop für PC Hardware, Software, Freifunkrouter und mehr. Von Mindfactory erhalten wir Unterstützung beim Kauf von Hardware und Komponenten.</p>
+     </div>
+      
+     <div class="col-sm-4 col-lg-3">
+       <a class="thumbnail" href="http://www.sublab.org" target="_window">
+         <img src="img/Banner/banner_sublab.svg"  alt="Sublab" style="width:40%"/>
+       </a>
+       <p>Das Sublab ist eine Freifunkkeimzelle in Leipzig und neue Heimat unserer Gründerväter
+       </p>
+     </div>
+     
+     <div class="clearfix visible-sm"></div>
+      
+     <div class="col-sm-4 col-lg-3 offset3">
+       <a class="thumbnail" href="http://www.maschinenraum.tk" target="_window"><img src="img/Banner/maschinenraum.png" alt="Maschinenraum" /></a>
+       <p>Der Maschinenraum ist unsere Basis und bietet uns Platz für Testorgien und unsere wöchentlichen Treffen.</p>
+     </div>
+    
+     <div class="col-sm-4 col-lg-3">
+       <a class="thumbnail" href="http://www.bittorf-wireless.de" target="_window"><img src="img/Banner/banner_bittorfwireless.png" alt="Bittorf Wireless" /></a>
+       <p>Bittorf Wireless unterstützt uns mit Technik, Wissen und Softwareentwicklung.</p>
+     </div>
+    
+     <div class="col-sm-4 col-lg-3">
+       <a class="thumbnail" href="http://www.cstorch.de" target="_window"><img src="img/Banner/cstorch_banner.jpg" alt="christian storch - Informationstechnologie" /></a>
+       <p>Christian Storch Informationstechnologie hilft uns bei Softwareentwicklung, Support und Installationen.</p>
+     </div>
+    
+     <div class="col-sm-4 col-lg-3">
+       <a class="thumbnail" href="http://www.weimar.de" target="_window"><img src="img/Banner/weimar_logo_print.png" alt="Weimar. Kulturstadt Europas" /></a>
+       <p>Wir arbeiten mit der Stadt Weimar zusammen und finden gemeinsam neue Standorte, die mit Weimarnetz versorgt werden sollen.</p>
+     </div>
+  	</div>
     
     
     <?php include("./inc/footer.inc.php") ?>
