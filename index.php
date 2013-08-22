2a284e40e2d42c551e8effc9a89d597c2b57c59a
Max F. Albrecht <1@178.is>
2013-08-22 02:37:36 +0200

    [refactor] home: ad space on right side with nested grid
    NOW it breaks ok

--- index.php
+++ index.php
@@ -56,62 +56,74 @@
     <!-- hier beginnt die eigentliche seite -->
     <div class="container">
 
-      <!-- eine reihe blöcke -->
+      <!-- main reihe blöcke - content/ad -->
       <div class="row">
+    
+        <div class="col-sm-11">
+    
+          <!-- eine reihe content blöcke -->
+          <div class="row">
+    
+            <!-- ein halber block (12/6=1/2) -->
+            <div class="col-sm-6">
+              <h2>Aktuelle Meldungen</h2>
+              <!-- dyn: news-liste -->
+              <?php
+              $wikinews=file_get_contents("http://wireless.subsignal.org/index.php?title=Vorlage:Startseite.Aktuelles");
+              $wikinews=substr($wikinews, strpos($wikinews, "<ul><li>" ), strpos($wikinews, "</li></ul>")-strpos($wikinews, "<ul><li>") + strlen("</li></ul>" ));
+              echo str_replace("/index.php?title=","http://wireless.subsignal.org/index.php?title",str_replace("<a href=", "<a target=_blank href=",$wikinews) );
+              ?>
+      
+              <p><a class="btn btn-xs btn-default" href="http://wireless.subsignal.org/index.php?title=Vorlage:Startseite.Aktuelles&action=edit" target="_blank">Text bearbeiten &raquo;</a></p>
+            </div>
+    
+            <!-- ein halber block (12/6=1/2) -->
+            <div class="col-sm-6">
+              <h2>Aktuelle Diskussionen</h2>
+              <p>
+                <!-- dyn: discuss-liste -->
+                <?php
+                echo str_replace("<a href=", "<a target=_blank href=",file_get_contents("http://weimarnetz.de/latestnews.html"));
+                ?>
         
-        <!-- ein halber block (12/6=1/2) -->
-        <div class="col-sm-6">
-          <h2>Aktuelle Meldungen</h2>
-          <!-- dyn: news-liste -->
-          <?php
-            $wikinews=file_get_contents("http://wireless.subsignal.org/index.php?title=Vorlage:Startseite.Aktuelles");
-            $wikinews=substr($wikinews, strpos($wikinews, "<ul><li>" ), strpos($wikinews, "</li></ul>")-strpos($wikinews, "<ul><li>") + strlen("</li></ul>" ));
-            echo str_replace("/index.php?title=","http://wireless.subsignal.org/index.php?title",str_replace("<a href=", "<a target=_blank href=",$wikinews) );
-          ?>
-          
-          <p><a class="btn btn-xs btn-default" href="http://wireless.subsignal.org/index.php?title=Vorlage:Startseite.Aktuelles&action=edit" target="_blank">Text bearbeiten &raquo;</a></p>
-        </div>
-        
-        <!-- ein halber block (12/6=1/2) -->
-        <div class="col-sm-6">
-          <h2>Aktuelle Diskussionen</h2>
-          <p>
-            <!-- dyn: discuss-liste -->
-            <?php
-              echo str_replace("<a href=", "<a target=_blank href=",file_get_contents("http://weimarnetz.de/latestnews.html"));
-            ?>
-            
-          </p>
-          <p><a class="btn btn-xs btn-default" href="./news.php" target="_blank" >Direkt zum Newsserver</a> <small>(Benutzer: <code>freifunk</code> Passwort: <code>weimar</code>)</small></p>
-       </div>
-       
-     </div> <!-- ende reihe -->
+              </p>
+              <p><a class="btn btn-xs btn-default" href="./news.php" target="_blank" >Direkt zum Newsserver</a> <small>(Benutzer: <code>freifunk</code> Passwort: <code>weimar</code>)</small></p>
+            </div>
+   
+          </div> <!-- ende reihe -->
 
-     <!-- zweite reihe blöcke -->
-     <div class="row">
+          <!-- zweite reihe blöcke -->
+          <div class="row">
 
-        <div class="col-sm-6">
-          <h2>Spendenaufruf</h2>
-          <!-- dyn: spenden-liste -->
-          <?php
-              	$wikinews=file_get_contents("http://wireless.subsignal.org/index.php?title=Vorlage:Spendenaufruf");
-                $wikinews=substr($wikinews, strpos($wikinews, "<ul><li>" ), strpos($wikinews, "</li></ul>")-strpos($wikinews, "<ul><li>") + strlen("</li></ul>" ));
-                echo str_replace("/index.php?title=","http://wireless.subsignal.org/index.php?title",str_replace("<a href=", "<a target=_blank href=",$wikinews) );
-          ?>
-          
-          <!-- paypal button -->
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_s-xclick">
-          <input type="hidden" name="hosted_button_id" value="MN595Q3HPXZVY">
-          <input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
-          <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-          </form>
-          
-          <!-- <p><a class="btn btn-default" href="#">View details &raquo;</a></p> -->
-        </div>
-        
-        <div class="col-sm-2 col-sm-offset-4">
-          <h2><span class="text-muted">Werbung</span></h2>
+            <div class="col-sm-6">
+              <h2>Spendenaufruf</h2>
+              <!-- dyn: spenden-liste -->
+              <?php
+              $wikinews=file_get_contents("http://wireless.subsignal.org/index.php?title=Vorlage:Spendenaufruf");
+              $wikinews=substr($wikinews, strpos($wikinews, "<ul><li>" ), strpos($wikinews, "</li></ul>")-strpos($wikinews, "<ul><li>") + strlen("</li></ul>" ));
+              echo str_replace("/index.php?title=","http://wireless.subsignal.org/index.php?title",str_replace("<a href=", "<a target=_blank href=",$wikinews) );
+              ?>
+      
+              <!-- paypal button -->
+              <ul><li>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                  <span class="label label-primary" style="vertical-align:text-top">PayPal:</span> 
+                  <input type="hidden" name="cmd" value="_s-xclick">
+                  <input type="hidden" name="hosted_button_id" value="MN595Q3HPXZVY">
+                  <input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
+                  <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
+                </form>
+              </li></ul>
+      
+              <!-- <p><a class="btn btn-default" href="#">View details &raquo;</a></p> -->
+            </div>
+    
+          </div> <!-- ende reihe -->
+
+        </div> <!-- ende content -->
+    
+        <div class="col-sm-1">
+          <h2><small class="text-muted">Werbung</small></h2>
           <!-- mindfactory banner -->
           <img src="img/Banner/120x500_mindfactory_hardware.jpg" width="120" height="500" alt="Ihre Hardware stößt an ihre Grenzen? Zeit für ein Upgrade! www.mindfactory.de" usemap="#admap" />
           <map name="admap">
@@ -123,11 +135,11 @@
             <area shape="rect" coords="3,413,116,438" target="_blank" href="http://www.mindfactory.de" />
           </map>
         </div>
-        
-      </div> <!-- ende reihe -->
+      
+      </div> <!-- ende main -->
 
       <?php include("./inc/footer.inc.php") ?>
-      
+  
     </div> <!-- ende inhalt /container -->
 
 
