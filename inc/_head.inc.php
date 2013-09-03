06600526a38c5691a88ac822eba4cdbb4d2eb554
andibraeu <ab@andi95.de>
2013-09-03 22:40:01 +0200

    added directory generator, fixed relative links

--- inc/_head.inc.php
+++ inc/_head.inc.php
@@ -4,8 +4,8 @@
 <meta name="author" content="Weimarnetz e.V." >
 
 <!-- Le styles -->
-<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="bower_components/bootstrap-theme-freifunk/dist/css/bootstrap.min.css" rel="stylesheet"> 
+<link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/bower_components/bootstrap-theme-freifunk/dist/css/bootstrap.min.css" rel="stylesheet"> 
 
 <!-- local fixes. -->
 <style type="text/css">
@@ -16,12 +16,12 @@
 
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
-  <script src="bower_components/bootstrap/assets/js/html5shiv.js"></script>
-  <script src="bower_components/bootstrap/assets/js/respond.min.js"></script>
+  <script src="/bower_components/bootstrap/assets/js/html5shiv.js"></script>
+  <script src="/bower_components/bootstrap/assets/js/respond.min.js"></script>
 <![endif]-->
 
 <!-- Le fav and touch icons -->
-<link rel="shortcut icon" href="img/favicon.ico">
+<link rel="shortcut icon" href="/img/favicon.ico">
 <link rel="apple-touch-icon" href="http://twitter.github.com/bootstrap/examples/images/apple-touch-icon.png">
 <link rel="apple-touch-icon" sizes="72x72" href="http://twitter.github.com/bootstrap/examples/images/apple-touch-icon-72x72.png">
 <link rel="apple-touch-icon" sizes="114x114" href="http://twitter.github.com/bootstrap/examples/images/apple-touch-icon-114x114.png">
