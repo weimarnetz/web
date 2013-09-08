4e2aa09757067d1dd7a0ba4344065d6800695320
andibraeu <ab@andi95.de>
2013-09-08 17:18:56 +0200

    footer: image link fixed

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -2,7 +2,7 @@
   <hr>
   <div class="col-sm-2">
     <p>
-      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="img/cc.png" /></a>
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="/img/cc.png" /></a>
     </p>
   </div>
   <div class="col-sm-10">
