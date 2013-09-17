48a1519e6a2fb0d23eeccbe42ee74dd2ecc71dc0
andibraeu <ab@andi95.de>
2013-09-17 16:47:26 +0200

    footer: less is more

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,15 +1,11 @@
 <footer class="row">
   <hr>
-  <div class="col-sm-2">
-    <p>
-      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="/img/cc.png" /></a>
-    </p>
-  </div>
-  <div class="col-sm-10">
-    <p><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Homepage Weimarnetz e.V.</span> von <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Weimarnetz e.V.</span> steht unter der Lizenz:
-      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/">Creative Commons Namensnennung-Nicht-kommerziell-Weitergabe unter gleichen Bedingungen 3.0 Deutschland</a>.
-    </p>
-    <p>Beruht auf einem Inhalt unter <a xmlns:dct="http://purl.org/dc/terms/" href="http://www.weimarnetz.de" rel="dct:source">www.weimarnetz.de</a>
-    </p>
+    <center>
+      http://www.weimarnetz.de
+      -  
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="/img/cc.png" /></a> 
+      - 
+      <a href="/impressum">Impressum</a>
+    </center>
   </div>
 </footer>
