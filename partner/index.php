7ffee4562b8f46c36fee79c883f85f56152bcb2c
andibraeu <ab@andi95.de>
2013-09-17 18:54:07 +0200

    image path correction

--- partner/index.php
+++ partner/index.php
@@ -32,18 +32,18 @@
      </style>
      
   	 <div class="col-sm-4 col-lg-3">
-       <a class="thumbnail" href="http://www.openwrt.org" target="_window"><img src="img/Banner/openwrt-logo.png" alt="OpenWrt" longdesc="OpenWrt schafft mit seiner Firmware für viele verschiedene WLAN-Router die Grundlagen für ein funktionierendes Netz" /></a>
+       <a class="thumbnail" href="http://www.openwrt.org" target="_window"><img src="/img/Banner/openwrt-logo.png" alt="OpenWrt" longdesc="OpenWrt schafft mit seiner Firmware für viele verschiedene WLAN-Router die Grundlagen für ein funktionierendes Netz" /></a>
        <p>OpenWrt ermöglich uns mit seiner Firmware, unsere Router für unseren Einsatzzweck perfekt anzupassen. Durch die stetige Weiterentwicklung der OpenWrt-Community sind wir bestens für die Zukunft gerüstet.</p>
      </div>
       
      <div class="col-sm-4 col-lg-3">
-       <a class="thumbnail" href="http://www.mindfactory.de" target="_window"><img src="img/Banner/mf_blue_on_white_logo.png" alt="Mindfactory.de" /></a>
+       <a class="thumbnail" href="http://www.mindfactory.de" target="_window"><img src="/img/Banner/mf_blue_on_white_logo.png" alt="Mindfactory.de" /></a>
        <p>Mindfactory ist der Onlineshop für PC Hardware, Software, Freifunkrouter und mehr. Von Mindfactory erhalten wir Unterstützung beim Kauf von Hardware und Komponenten.</p>
      </div>
       
      <div class="col-sm-4 col-lg-3">
        <a class="thumbnail" href="http://www.sublab.org" target="_window">
-         <img src="img/Banner/banner_sublab.svg"  alt="Sublab" style="width:40%"/>
+         <img src="/img/Banner/banner_sublab.svg"  alt="Sublab" style="width:40%"/>
        </a>
        <p>Das Sublab ist eine Freifunkkeimzelle in Leipzig und neue Heimat unserer Gründerväter
        </p>
@@ -52,24 +52,24 @@
      <div class="clearfix visible-sm"></div>
       
      <div class="col-sm-4 col-lg-3 offset3">
-       <a class="thumbnail" href="http://www.maschinenraum.tk" target="_window"><img src="img/Banner/maschinenraum.png" alt="Maschinenraum" /></a>
+       <a class="thumbnail" href="http://www.maschinenraum.tk" target="_window"><img src="/img/Banner/maschinenraum.png" alt="Maschinenraum" /></a>
        <p>Der Maschinenraum ist unsere Basis und bietet uns Platz für Testorgien und unsere wöchentlichen Treffen.</p>
      </div>
     
      <div class="clearfix visible-lg"></div>
     
      <div class="col-sm-4 col-lg-3">
-       <a class="thumbnail" href="http://www.bittorf-wireless.de" target="_window"><img src="img/Banner/banner_bittorfwireless.png" alt="Bittorf Wireless" /></a>
+       <a class="thumbnail" href="http://www.bittorf-wireless.de" target="_window"><img src="/img/Banner/banner_bittorfwireless.png" alt="Bittorf Wireless" /></a>
        <p>Bittorf Wireless unterstützt uns mit Technik, Wissen und Softwareentwicklung.</p>
      </div>
     
      <div class="col-sm-4 col-lg-3">
-       <a class="thumbnail" href="http://www.cstorch.de" target="_window"><img src="img/Banner/cstorch_banner.jpg" alt="christian storch - Informationstechnologie" /></a>
+       <a class="thumbnail" href="http://www.cstorch.de" target="_window"><img src="/img/Banner/cstorch_banner.jpg" alt="christian storch - Informationstechnologie" /></a>
        <p>Christian Storch Informationstechnologie hilft uns bei Softwareentwicklung, Support und Installationen.</p>
      </div>
     
      <div class="col-sm-4 col-lg-3">
-       <a class="thumbnail" href="http://www.weimar.de" target="_window"><img src="img/Banner/weimar_logo_print.png" alt="Weimar. Kulturstadt Europas" /></a>
+       <a class="thumbnail" href="http://www.weimar.de" target="_window"><img src="/img/Banner/weimar_logo_print.png" alt="Weimar. Kulturstadt Europas" /></a>
        <p>Wir arbeiten mit der Stadt Weimar zusammen und finden gemeinsam neue Standorte, die mit Weimarnetz versorgt werden sollen.</p>
      </div>
   	</div>
