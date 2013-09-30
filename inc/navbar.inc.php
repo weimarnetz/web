67d9359063f72e51ac686ec97896a565defed684
andibraeu <ab@andi95.de>
2013-09-30 11:18:57 +0200

    navbar: vollqualifizierte Links

--- inc/navbar.inc.php
+++ inc/navbar.inc.php
@@ -28,27 +28,27 @@
         <a href="/wiki" class="dropdown-toggle" data-toggle="dropdown">Informationen <b class="caret"></b></a>
         <ul class="dropdown-menu">
           <li class="dropdown-header">Einstieg</li>
-          <li <?php echo getActiveClass("/mitmachen")?> ><a href="/mitmachen">Mitmachen</a></li> 
-          <li <?php echo getActiveClass("/contact")?> ><a href="/contact">Community</a></li>
-          <li <?php echo getActiveClass("/about")?> ><a href="/about">Selbstdarstellung</a></li>
+          <li <?php echo getActiveClass("/mitmachen")?> ><a href="http://www.weimarnetz.de/mitmachen">Mitmachen</a></li> 
+          <li <?php echo getActiveClass("/contact")?> ><a href="http://www.weimarnetz.de/contact">Community</a></li>
+          <li <?php echo getActiveClass("/about")?> ><a href="http://www.weimarnetz.de/about">Selbstdarstellung</a></li>
           <li class="dropdown-header">Dokumentation</li>
-          <li <?php echo getActiveClass("/wiki")?>><a href="/wiki">Wiki</a></li>
-          <li <?php echo getActiveClass("/newsgroups")?> ><a href="/newsgroups" >Mailinglisten</a></li>
+          <li <?php echo getActiveClass("/wiki")?>><a href="http://www.weimarnetz.de/wiki">Wiki</a></li>
+          <li <?php echo getActiveClass("/newsgroups")?> ><a href="http://www.weimarnetz.de/newsgroups" >Mailinglisten</a></li>
           <li class="dropdown-header">Rechtliches</li>
-          <li <?php echo getActiveClass("/termsofuse")?>><a href="/termsofuse" >Nutzungsbedingungen</a></li>
-          <li <?php echo getActiveClass("/picopeering")?>><a href="/picopeering" >Picopeering Aggreement</a></li>
+          <li <?php echo getActiveClass("/termsofuse")?>><a href="http://www.weimarnetz.de/termsofuse" >Nutzungsbedingungen</a></li>
+          <li <?php echo getActiveClass("/picopeering")?>><a href="http://www.weimarnetz.de/picopeering" >Picopeering Aggreement</a></li>
         </ul>
       </li>
       <li class="dropdown">
         <a href="/map" class="dropdown-toggle"  data-toggle="dropdown">Daten <b class="caret"></b></a>
         <ul class="dropdown-menu">
           <li class="dropdown-header">Status</li>
-          <li <?php echo getActiveClass("/status")?> ><a href="/status" >OLSR Status</a></li>   
-          <li <?php echo getActiveClass("/monitoring")?> ><a href="/monitoring" >Monitoring</a></li>
+          <li <?php echo getActiveClass("/status")?> ><a href="http://www.weimarnetz.de/status" >OLSR Status</a></li>   
+          <li <?php echo getActiveClass("/monitoring")?> ><a href="http://www.weimarnetz.de/monitoring" >Monitoring</a></li>
           <li class="dropdown-header">Karten</li>
-          <li <?php echo getActiveClass("/map2")?> ><a href="/map2">Geographische Karte (aktiv + inaktiv)</a></li>
-          <li <?php echo getActiveClass("/maplive")?> ><a href="/maplive">Geographische Karte (Live!)</a></li>
-          <li <?php echo getActiveClass("/topology")?> ><a href="/topology">Topologische Karte</a></li>
+          <li <?php echo getActiveClass("/map2")?> ><a href="http://www.weimarnetz.de/map2">Geographische Karte (aktiv + inaktiv)</a></li>
+          <li <?php echo getActiveClass("/maplive")?> ><a href="http://www.weimarnetz.de/maplive">Geographische Karte (Live!)</a></li>
+          <li <?php echo getActiveClass("/topology")?> ><a href="http://www.weimarnetz.de/topology">Topologische Karte</a></li>
         </ul>          
       </li>
       <li class="dropdown">
@@ -56,21 +56,21 @@
         <ul class="dropdown-menu">
           <li class="dropdown-header">Firmware</li>
 	  <li <?php echo getActiveClass("/meshkit")?> ><a href="http://meshkit.weimarnetz.de" >Eigene Firmware bauen</a></li>
-	  <li <?php echo (strpos(dirname($_SERVER["SCRIPT_NAME"]), "/freifunk/firmware/ar71xx") !== false) ? "class='active'":"" ;?> ><a href="/freifunk/firmware/ar71xx" >Atheros (TP-Link, Ubiqity, usw)</a></li>   
-          <li <?php echo getActiveClass("freifunk/firmware/brcm47xx")?> ><a href="/freifunk/firmware/brcm47xx" >Broadcom (Linksys, Buffalo, usw.)</a></li>
-          <li <?php echo getActiveClass("freifunk/firmware/nightlies")?> ><a href="/freifunk/firmware/nightlies" >Nightly Builds</a></li>
+	  <li <?php echo (strpos(dirname($_SERVER["SCRIPT_NAME"]), "/freifunk/firmware/ar71xx") !== false) ? "class='active'":"" ;?> ><a href="http://www.weimarnetz.de/freifunk/firmware/ar71xx" >Atheros (TP-Link, Ubiqity, usw)</a></li>   
+          <li <?php echo getActiveClass("freifunk/firmware/brcm47xx")?> ><a href="http://www.weimarnetz.de/freifunk/firmware/brcm47xx" >Broadcom (Linksys, Buffalo, usw.)</a></li>
+          <li <?php echo getActiveClass("freifunk/firmware/nightlies")?> ><a href="http://www.weimarnetz.de/freifunk/firmware/nightlies" >Nightly Builds</a></li>
         </ul>
       </li>
       <li class="divider-vertical"></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li <?php echo getActiveClass("/partner")?> ><a href="/partner">Partner</a></li>
+      <li <?php echo getActiveClass("/partner")?> ><a href="http://www.weimarnetz.de/partner">Partner</a></li>
       <li class="dropdown">
         <a href="/impressum.php" class="dropdown-toggle"  data-toggle="dropdown">Impressum <b class="caret"></b>
         </a>
         <ul class="dropdown-menu">
-          <li <?php echo getActiveClass("/impressum")?> ><a href="/impressum">Impressum</a></li>
-          <li <?php echo getActiveClass("/contact")?> ><a href="/contact">Kontakt</a></li>
+          <li <?php echo getActiveClass("/impressum")?> ><a href="http://www.weimarnetz.de/impressum">Impressum</a></li>
+          <li <?php echo getActiveClass("/contact")?> ><a href="http://www.weimarnetz.de/contact">Kontakt</a></li>
         </ul>
       </li>
     </ul>
