fc3eb94cfdd5204282808d9d46895d204e7ef806
andibraeu <ab@andi95.de>
2013-10-06 15:47:43 +0200

    footer: added social media icons from http://icons.mysitemyway.com/glossy-black-icons-social-media-logos/

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -5,4 +5,9 @@
   <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="/img/cc.png" /></a> 
   - 
   <a href="/impressum">Impressum</a>
+  <br/><br/>
+  <a href="mailto:wireless@subsignal.org" target="_window" style="margin-right: 15px"><img alt="Email ans Weimarnetz" width="30px" src="/img/icons/mail.png" /></a> 
+  <a href="http://www.facebook.com/weimarnetz" target="_window" style="margin-right: 15px"><img alt="Weimarnetz im Facebook" width="30px" src="/img/icons/facebook.png" /></a> 
+  <a href="http://www.twitter.com/weimarnetz" target="_window" style="margin-right: 15px"><img alt="Weimarnetz auf Twitter" width="30px" src="/img/icons/twitter.png" /></a> 
+  <a href="/inc/feed/feed.php" target="_window" style="margin-right: 15px"><img alt="Aggregierte Newsfeed des Weimarnetz" width="30px" src="/img/icons/rss.png" /></a> 
 </footer>
