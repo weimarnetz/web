ff33088d325cf68516d73232fa1d29f186f5c786
Max F. Albrecht <1@178.is>
2013-11-08 20:51:45 +0100

    site: add an Error 404 page
    activate with:
    `server.errorfile-prefix = "/srv/www/htdocs/errors/status-"`
    
    as per <http://redmine.lighttpd.net/projects/lighttpd/wiki/Server_errorfile-prefixDetails>

---
+++ errors/404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Weimarnetz e.V.</title>
+    <?php include("../inc/_head.inc.php") ?>  
+  </head>
+
+  <body id="home">
+
+    <?php include("../inc/navbar.inc.php") ?>
+
+    <!-- grosses startseitenbanner -->
+    <div id="banner" class="jumbotron jumbotron-special alert-danger ff-logo-bg">
+      <div class="container">
+        <h1>Error 404!</h1>
+        <p class="lead">Diese Seite wurde leider nicht gefunden!</p>
+        <p>
+          <a class="btn btn-success btn-lg" href="/">Zur Startseite &raquo;</a>
+          <a class="btn btn-primary btn-lg" href="/spenden">Jetzt spenden &raquo;</a>
+        </p>
+        
+      </div>
+    </div>
+
+    <?php include("../inc/footer.inc.php") ?>
+
+    <?php include("../inc/_foot.inc.php") ?>
+
+  </body>
+</html>
