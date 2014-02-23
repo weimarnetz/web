84f151072054213664c2a7806a27c8d8476e2fc6
andibraeu <ab@andi95.de>
2014-02-23 21:50:35 +0100

    monitoring: weitere details hinzugefgt

--- monitoring/index.php
+++ monitoring/index.php
@@ -39,7 +39,7 @@
 
 </div>
 	
-<script src="../underscore.js"></script>
+<script src="/inc/underscore.js"></script>
   <?php include("../inc/footer.inc.php") ?>
 
 </div>
@@ -51,9 +51,9 @@
         <td class="<%= lastChangeColor(item.doc.mtime) %>"><%= item.doc.mtime  %></td>
         <td><a href="#" data-toggle="modal" data-target="#info<%= key %>"><%= item.doc.hostname %></a></td>
 	  <% if (item.doc.olsr.ipv4Config) {%> 
-	    <td data-nodenumer="<%= item.doc.nodeNumber %>" data-mainip="<%= item.doc.olsr.ipv4Config.mainIpAddress %>">
+	    <td data-nodenumer="<%= item.doc.weimarnetz.nodenumber %>" data-mainip="<%= item.doc.olsr.ipv4Config.mainIpAddress %>">
 	      <a href="http://<%= item.doc.olsr.ipv4Config.mainIpAddress %>/cgi-bin-status.html" target="_blank">
-	      <%= item.doc.nodeNumber %>
+	      <%= item.doc.weimarnetz.nodenumber %>
 	      </a><% } else { %>
 	    <td>
 	  <% } %>
@@ -77,13 +77,21 @@
           </div>
           <div class="modal-body">
 	    <ul class="nav nav-tabs" id="dev<%= key%>">
-	      <li class="active"><a href="#device<%= key %>" data-toggle="pill">Gerät</a></li>
+	      <li class="active"><a href="#general<%= key %>" data-toggle="pill">Allgemein</a></li>
+	      <li><a href="#contact<%= key %>" data-toggle="pill">Kontakt</a></li>
 	      <li><a href="#network<%= key %>" data-toggle="pill">Netzwerk</a></li>
 	      <li><a href="#olsr<%= key %>" data-toggle="pill">OLSR</a></li>
+	      <li><a href="#map<%= key %>" data-toggle="pill">Karte</a></li>
 	    </ul>
 	    <div class="tab-content">
-	      <div class="tab-pane fade in active" id="device<%= key %>">
-		<dl><dt>Bezeichnung</dt><dd><%= item.doc.hardware %></dd></dl>
+	      <div class="tab-pane fade in active" id="general<%= key %>">
+		<dl>
+		  <dt>Gerät</dt><dd><%= item.doc.system.sysinfo[1] %></dd>
+		  <dt>Hardware</dt><dd><%= item.doc.hardware %></dd>
+		  <dt>nächster Nachbar</dt><dd><%= item.doc.weimarnetz.nexthop %></dd>
+		  <dt>Gateway</dt><dd><%= item.doc.weimarnetz.gateway %></dd>
+		  <dt>Gatewaykosten</dt><dd><%= item.doc.weimarnetz.gatewaycost %></dd>
+		</dl>
 	      </div>
 	      <div class="tab-pane fade in" id="network<%= key %>">
 		<dl>
@@ -94,12 +102,32 @@
 		 }) %>
 		</dl>
 	      </div>
+	      <div class="tab-pane fade in" id="contact<%= key %>">
+		<dl>
+		  <% if (item.doc.freifunk.contact.nickname) { %>
+		    <dt>Ansprechpartner</dt><dd><%= item.doc.freifunk.contact.nickname %></dd>
+		  <% } %>
+		  <% if (item.doc.freifunk.contact.mail) { %>
+		    <dt>Email</dt><dd><%= item.doc.freifunk.contact.mail %></dd>
+		  <% } %>
+		  <% if (item.doc.freifunk.contact.phone) { %>
+		    <dt>Telefon</dt><dd><%= item.doc.freifunk.contact.phone %></dd>
+		  <% } %>
+		  <% if (item.doc.location) { %>
+		    <dt>Standort</dt><dd><a href="http://map.weimarnetz.de/#detail?node=<%= item.id %>" target="_blank"><%= item.doc.location %></a></dd>
+		  <% } %>
+		</dl>
+	      </div>
 	      <div class="tab-pane fade in" id="olsr<%= key %>">
 		<dl>
 		<% _.each(item.doc.olsr.links,function(olinks,olsrKey,olsrList) {%>
 		  <dt><%= olinks.destNodeId%></dt><dd><%=olinks.destAddr%></dd>
 		<% }) %>
 		</dl>
+	      </div>
+	      <div class="tab-pane fade in" id="map<%= key%>">
+		<iframe src="" width="100%" height="500px"></iframe>
+		<!--<iframe src="http://map.weimarnetz.de/map.html?bbox=<%= item.doc.longitude %>,<%= item.doc.latitude %>" width="100%" height="500px"></iframe>-->
 	      </div>
 	    </div>
           </div>
@@ -111,6 +139,7 @@
     </div>
     <% }}) %>
 </script>
+
 
 
 <?php include("../inc/_foot.inc.php") ?>
@@ -130,9 +159,13 @@ success: ( function(Response){
   _.each(rows, function(item, key, list) {
     var diff = new Date().getTime() - new Date(item.doc.mtime).getTime();
     item.doc.mtime = Math.round(diff/36000000);
-    if (item.doc.olsr && item.doc.olsr.ipv4Config) {
-      item.doc.nodeNumber = getNodeNumber(item.doc.olsr.ipv4Config.mainIpAddress);
+    if (! item.doc.weimarnetz) {
+      item.doc.weimarnetz = {};
     }
+    if (! item.doc.weimarnetz.nodenumber && item.doc.olsr && item.doc.olsr.ipv4Config) {
+      item.doc.weimarnetz.nodenumber = getNodeNumber(item.doc.olsr.ipv4Config.mainIpAddress);
+    }
+    if (! item.doc.weimarnetz.nodenumber) { item.doc.weimarnetz.nodenumber = NaN; }
   });
   console.log(26, 'sorted: ', rows);
   $("table.outer tbody").html(_.template(tableTemplate,{items:rows}));
@@ -146,16 +179,16 @@ test()
 
 function getNodeNumber(ip) {
   ip = ip.split(".");
-  return Math.round(ip[2]) + ( Math.round(ip[3] / 64) -1 ) * 255;
+  return Math.floor(ip[2]) + ( Math.floor(ip[3] / 64)  ) * 255;
 }
 
 function lastChangeColor(mtime) {
   if (mtime<=12) {
     return "success";
   } else if (mtime<=96) {
-    return "warning";
+    return "info";
   } else if (mtime<=192) {
-    return "danger";
+    return "warning";
   } else {
     return "danger";
   }
