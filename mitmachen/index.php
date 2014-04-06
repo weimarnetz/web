014c877319b7894de7109e8e37556a6ef787d406
mr <mr@m18.uni-weimar.de>
2014-04-06 16:08:56 +0200

    ueberarbeitung und umstruktuierung der mitmachseite - Sa, 06.04.2014

--- mitmachen/index.php
+++ mitmachen/index.php
@@ -9,59 +9,104 @@
   <?php include("../inc/navbar.inc.php") ?>
 
   <div class="container">
+<!--<h3>Warum mitmachen?</h3>-->
+<div class="row">
+	<div class="col-sm-4 col-lg-4">
+		<div class="case" id="case-level2">
+			<div class="case-head page-header">
+				<h4>Freies Internet für alle</h4>
+				<img class="img-responsive"  src="../img/participate/participate_medium.png">
+			</div>
+			<div class="case-body">
+				<p>Stelle deinen Internetanschluss einfach und sicher für andere zur Verfügung. Ideal für Cafés oder Arbeitsräume. Auch für Touristen sind offene und kostenlose Hotspots praktisch.</p>
+			</div>
+		</div>
+	</div>
 
-    <h3>Wir freuen uns über dein Interesse am Weimarnetz.</h3>
-    <strong>Jeder kann mitmachen!</strong>
+	<div class="col-sm-4 col-lg-4">
+		<div class="case" id="case-backbone">
+			<div class="case-head page-header">
+				<h4>Hilf mit beim Netzausbau</h4>
+				<img class="img-responsive"  src="../img/participate/participate_big.png">
+			</div>
+			<div class="case-body">
+				<p>Erweitere das Netz durch hohe und exponierte Standorte. Unter oder auch auf dem Dach montiert, können die sich Router besser direkt miteinander verbinden. Das Netz wird so stabiler und ausfallsicherer.</p>
+			</div>
+		</div>
+	</div>
 
-    <p>
-      Um am Weimarnetz und am Freifunk teilzunehmen brauchst du entweder eine Funkverbindung - besser noch eine Sichtverbindung - zu einem bestehenden Knotenpunkt, oder du stellst selber einen Router (auch <em>Knotenpunkt</em> genannt) auf und verbindest ihn mit dem Internet.<br \><a href=""><!--FIXME - wiki, liste unterstuetzter modelle-->Einen passenden Router</a> kannst du im Internet oder vor Ort z.B. beim Computer Center Matthey, Saturn, MediaMarkt, usw. bestellen und kaufen.
-    </p>
+	<div class="col-sm-4 col-lg-4">
+		<div class="case" id="case-level2">
+			<div class="case-head page-header">
+				<h4>Nutze die Freifunk-Dienste</h4>
+				<img class="img-responsive"  src="../img/participate/participate_small.png">
+			</div>
+			<div class="case-body">
+				<p>Entdecke Freifunk und nutze angebotene Dienste im Netzwerk. Du kannst auch selber Dienste von zuhause aus betreiben. Z.B. einen Webserver oder ein Radio-Streaming.</p>
+			</div>
+		</div>
+	</div>
+</div>
 
-    <p>
-      </h4>Die von uns verwendeten und empholenen Modelle für den einfachen Einstieg sind:</h4>
-      <ul>
-        <li>TP-Link <a href="http://www.heise.de/preisvergleich/tp-link-tl-wr841n-a340658.html">TL-WR841N</a>, <a href="http://www.heise.de/preisvergleich/tp-link-tl-wr841nd-a601787.html">TL-WR841ND</a> und <a href="http://www.heise.de/preisvergleich/tp-link-tl-wr842nd-a702920.html">TL-WR842ND</a> (15 bis 30 &euro;)</li>
-        <li>TP-Link <a href="http://www.heise.de/preisvergleich/tp-link-tl-wdr3600-a806392.html">TL-WDR3600 und <a href="http://www.heise.de/preisvergleich/tp-link-tl-wdr4300-a772368.html">TL-WDR4300</a> (45 bis 55 &euro;)</li>
-        <li>Ubiquiti <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m2-2-4ghz-a651912.html">NanoStation M2</a> &amp; <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m2-loco-a651904.html">Loco</a>, <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m5-5ghz-a651909.html">NanoStation M5</a>  &amp; <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m5-loco-a651908.html">Loco</a>, oder <a href="http://www.heise.de/preisvergleich/ubiquiti-nanobridge-m5-25db-i-nb-5g25-a651918.html">NanoBridge M5</a> für Richtfunk bis zu 5 km (50 bis 80 &euro;)</li>
-      </ul>
-    </p>
+<h3>Wo kann ich mitmachen</h3>
+<p>Du kannst eienen Router quasi überall aufbauen. Am besten gibt es vor ein eine Möglichkeit den Router ans Internet anzuschließen oder es gibt bereits WLAN-Funk-Nachbarn.</p>
+<p>Sieh auf der Karte nach ob bereits Knotenpunkte in deiner Nachbarschaft sind mit denen sich dein Router verbinden kann. Du kannst auch zusammen mit anderen einen Aufbau planen. Das <!--FIXME: Wiki-Seite vorher ueberarbeiten-->Weimarnetz-Treffen findet jeden Diestags im <a href="http://blog.maschinenraum.tk/contact/">Maschinenraum</a> statt. Wenn du nicht zum Treffen kommen kannst, erreichst du den Verein auch über die <a href="http://wireless.subsignal.org/index.php?title=Mailingliste">Mailingliste</a> oder über die <a href="mailto:kontakt@weimarnetz.de">Kontakt-E-Mail-Adresse</a>.</p>
+<div style="text-align: center">
+<iframe id="ifrm" src="http://map.weimarnetz.de/map.html#bbox=50.9484779952583,11.194210052490234,51.00651822828961,11.441402435302734" width="98%" scrolling="yes" marginwidth="0" marginheight="0" frameborder="0" style="height: 400px;">Weimarnetz Knoten Karte</iframe>
+</div>
 
-    <p>
-      Es ist auch möglich Dienstags beim Treffen einen Router (TL-WR841) zubekommen (sofern welche auf Lager sind) oder sich an Sammelbestellungen zu beteiligen, die wir regelmäßig organisieren. Am besten schreibst du uns vorher eine <a href="mailto:kontakt@weimarnetz.de">E-Mail</a> oder hinterlässt eine Nachricht auf dem <a href="/contact">Anrufbeantworter</a>.
-    </p>
+<h3>Gibt es beim Router aufstellen etwas zu bachten?</h3>
+<dl>
+<dt>Die Teilname als Nutzer oder Knotenbetreiber setzt keine Registrierung, Anmeldung, Erlaubniss oder ähnliches vorraus!</dt>
+<dd>Das Netz ist frei und offen für jeden. Siehe dazu auch das <a href="http://www.picopeer.net/PPA-de.html">Pico Peering Agreement</a>.<br \>
+Jeder kann einfach so neue Router aufzubauen. Wir empfehlen Einsteigern dennoch zum Treffen zu kommen, denn vor Ort und persönlich könnnen offene Fragen geklärt und nützliche Tipps ausgetauscht werden. Wenn du möchtest kannst du dich auch auf der Mailingliste anmelden um über Treffen und Neuigkeiten informiert zu werden.</dd><br \>
+<dt>Wie ist das eigentlich rechtlich wenn ich mein Internetanschluss anderen zur Verfügung stelle?</dt>
+<dd>Um DSL-Einspeiser vor möglichen Abmahnungen zu schützen und von der sogenannten Störerhaftung zu entlasten betreibt der Weimarnetz e.V. einen VPN-Server in Berlin über den alle WLAN-Teilnehmer getunnelt werden.</dd>
+</dl>
 
-    <p>
-      Zusätzlich kannst du auf der <a href="/map2">Karte</a> sehen, wo in deiner Nachbarschaft bereits Router aufgebaut sind. Aber selbst wenn du den ersten Knoten in deiner Nachbarschaft aufstellst, kannst du das Weimarnetz und andere angeschlossene Freifunk-Netze nutzen, da wir mit dem <a href="http://freifunk.net/blog/2014/02/das-intercity-vpn/">Intercity-VPN</a> verbunden sind.
-    </p>
-    
-    <p>
-      Die Teilname als Nutzer oder Knotenbetreiber setzt keine Registrierung, Anmeldung, Erlaubniss oder ähnliches vorraus! Das Netz ist frei und offen für jeden. Siehe dazu auch das <a href="http://www.picopeer.net/PPA-de.html">Pico Peering Agreement</a>. Wir empfehlen Einsteigern dennoch zum Treffen zu kommen, denn vor Ort und persönlich könnnen offene Fragen geklärt und nützliche Tipps ausgetauscht werden. Wenn du möchtest kannst du dich auch auf der Mailingliste anmelden um über Treffen und Neuigkeiten informiert zu werden.
-    </p>
+<h3>Welche Router kann ich verwenden?<br \>
+Wie und woher bekomme ich einen Router?</h3>
+<p><a href=""><!--FIXME - wiki, liste unterstuetzter modelle-->Einen passenden Router</a> kannst du entweder im Internet oder vor Ort beim <a href="http://www.ccm-weimar.de/">Computer Center Matthey</a>, Saturn, MediaMarkt, u.a. kaufen.
+</p>
 
-    <p>
-      Genug der Vorrede,
-      <h3>Ich habe meinen Router -- Wie geht es weiter -- Wie kommt die Weimarnetz-Firmware auf den Router?</h3>
-      Im Wiki findest du eine detailierte aber kurze Anleitung vom Auspacken bis zum Aufstellen deines Routers. Wenn du Hilfe benötigst oder dir etwas unklar ist, schreib uns auf der Mailingliste oder komm zum wöchtentlichen Treffen in den Maschinenraum, Marienstraße 18.
-    </p>
+<h5>Die von uns verwendeten und empholenen Modelle für den einfachen Einstieg sind:</h5>
+<p>
+<ul>
+	<li>TP-Link <a href="http://www.heise.de/preisvergleich/tp-link-tl-wr841n-a340658.html" target="_blank">TL-WR841N</a>, <a href="http://www.heise.de/preisvergleich/tp-link-tl-wr841nd-a601787.html" target="_blank">TL-WR841ND</a> und <a href="http://www.heise.de/preisvergleich/tp-link-tl-wr842nd-a702920.html" target="_blank">TL-WR842ND</a> (15 bis 30 &euro;)</li>
+	<li>TP-Link <a href="http://www.heise.de/preisvergleich/tp-link-tl-wdr3600-a806392.html" target="_blank">TL-WDR3600</a> und <a href="http://www.heise.de/preisvergleich/tp-link-tl-wdr4300-a772368.html" target="_blank">TL-WDR4300</a> (45 bis 55 &euro;)</li>
+	<li>Ubiquiti <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m2-2-4ghz-a651912.html" target="_blank">NanoStation M2</a> &amp; <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m2-loco-a651904.html" target="_blank">Loco</a>, <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m5-5ghz-a651909.html" target="_blank">NanoStation M5</a>  &amp; <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m5-loco-a651908.html" target="_blank">Loco</a>, oder <a href="http://www.heise.de/preisvergleich/ubiquiti-nanobridge-m5-25db-i-nb-5g25-a651918.html" target="_blank">NanoBridge M5</a> für Richtfunk bis zu 5 km (50 bis 80 &euro;)</li>
+	</ul>
+</p>
+<p>Es ist auch möglich Dienstags beim Treffen einen Router (TL-WR841) zubekommen (sofern welche auf Lager sind) oder sich an Sammelbestellungen zu beteiligen. Am besten schreibst du uns vorher eine <a href="mailto:kontakt@weimarnetz.de">E-Mail</a> oder hinterlässt eine Nachricht auf dem <a href="/contact">Anrufbeantworter</a>.
+</p>
 
-    <p>
-      <h3>Warum ueberhaupt mitmachen?</h3>
-      Es gibt viele gute Gruende am Weimarnetz und am Freifunk teilzunehmen.
-      <ul>
-        <li>Du kannst deinen Internetanschluss mit anderen teilen, um so zum Beispiel die Kosten für den Anschluss zuteilen, oder deinen Anschluss einfach und sicher für Touristen und Besucher bereitstellen (siehe Störerhaftung und VPN-Gateway).</li>
-        <li>Du förderst den Ausbau selbstverwalteter Netzwerk- und Internet-Infratstrukturen. Der Weimarnetz e.V. beteiligt sich an Freifunk.net und dem Intercity-VPN welches die verschiedenen Communitys vernetzt, so kannst du bspw. Dienste auch aus Hamburg, Berlin oder Leipzig nutzen.</li>
-        <li>Du kannst selber Dienste im Netz betreiben oder Dienste von anderen mitnutzen. <a href="/services">Im Weimarnetz angebotene Dienste</a></li>
-      </ul>
-    </p>
+<h3>Ich habe einen Router / Wie geht es weiter?<br \>
+Wie kommt die Weimarnetz-Firmware auf den Router?</h3>
+<p>Im Wiki findest du eine Anleitung wie du mit dem <a href="http://meshkit.weimarnetz.de/">Meshkit</a> die Firmware vorbereitest und dann auf dem Router <a href="http://wireless.subsignal.org/index.php?title=Firmware-Installation">installierst</a>. Wenn du Hilfe benötigst oder dir etwas unklar ist, schreib uns auf der Mailingliste oder komm zum wöchtentlichen Treffen in den Maschinenraum, Marienstraße 18.
+</p>
 
-    <p>
-      <h3>Ich habe von Technik nicht so viel Ahnung moechte euch aber dennoch gerne unterstützen, was kann ich tun?</h3>
-      <ul>
-        <li><strong>Werbung machen!</strong> Erzähle deinen Freunden, Arbeitskollegen, deiner Lieblingskneipe und anderen von der Freifunk-Idee und bring sie mit uns in Kontakt. Wir helfen euch gerne beim Aufstellen und Betreiben von Knotenpunkten.</li>
-        <li><strong>Spenden!</strong> Unterstütze den Verein mit einmaligen oder monatlichen <a href="/spenden/">Spenden</a>. Auf <a href="http://www.betterplace.org/de/projects/14895-weimarnetz-e-v-freies-wlan-in-weimar">Betterplace</a> kannst du auch für konkrete Projekte spenden. Als monatlicher Beitrag haben sich 5,00 &euro; etabliert. Diese werden z.B. für den Betrieb des Servers in Berlin bei <a href="https://www.in-berlin.de/verein/">IN-Berlin</a>und den Anschaffen von Routern, Kabeln, PoE-Adaptern usw. verwendet.</li>
-        <li><strong>Inhalte auf der Website und im Wiki verbesser und aktuallisieren!</strong> Wir sind meistens nicht die Kommunikationsexperten und freuen uns über Hilfe bei der Aussenwerbung und -Kommunikation, sowie bei besseren Erklärungen der technischen Zusammenhänge.</li>
-      </ul>
-    </p>
+<h3>Ich möchte verstehen wie die Firmware funktioniert und Änderungen vornehmen!</h3>
+<p>Programmierer und Entwickler können die <a href="https://github.com/weimarnetz/weimarnetz/blob/master/README.md">Firmware</a> und andere Weimarnetz-Software direkt auf <a href="https://github.com/weimarnetz/">Github</a> einsehen und bearbeiten. Dort und im Wiki beschrieben findest du alles um dir eine Entwicklungsumgebung aufzusetzen um dich an der Weiterentwicklung zu beteiligen.
+</p>
+
+<h3>Ich habe von Technik nicht so viel Ahnung möchte euch aber dennoch gerne unterstützen, was kann ich tun?</h3>
+<p>
+<ul>
+<li><strong>Werbung machen!</strong> Erzähle deinen Freunden, Arbeitskollegen, deiner Lieblingskneipe und anderen von der Freifunk-Idee und bring sie mit uns in Kontakt. Wir helfen euch gerne beim Aufstellen und Betreiben von Knotenpunkten.</li>
+<li><strong>Spenden!</strong> Unterstütze den Verein mit einmaligen oder monatlichen <a href="/spenden/">Spenden</a>. Auf <a href="http://www.betterplace.org/de/projects/14895-weimarnetz-e-v-freies-wlan-in-weimar">Betterplace</a> kannst du auch für konkrete Projekte spenden. Als monatlicher Beitrag haben sich 5,00 &euro; etabliert. Diese werden z.B. für den Betrieb des Servers in Berlin bei <a href="https://www.in-berlin.de/verein/">IN-Berlin</a> und den Anschaffen von Routern, Kabeln, PoE-Adaptern usw. verwendet.</li>
+<li><strong>Inhalte auf der Website und im Wiki verbesser und aktuallisieren!</strong> Wir sind meistens nicht die Kommunikationsexperten und freuen uns über Hilfe bei der Aussenwerbung und -Kommunikation, sowie bei besseren Erklärungen der technischen Zusammenhänge.</li>
+</ul>
+</p>
+
+<p>
+<h3>Weitere Informationen und Hinweise</h3>
+<dl><dt>Was ist ein Mesh-Netzwek? Was ist ein VPN?<dt>
+<dd>Wenn ein Router direkt oder indirekt über andere das Internet erreichen kann, wird er automatisch Teil des <em>Virtual Privat Networks</em>. Jeder Router der sich mit anderen Routern über Funk verbinden kann, bildet zusammen mit diesen ein Maschennetz (engl. Mesh-Network). Das Mesh-Net beschreibt vielmehr die besondere Eigenschaft des Netzwerks &mdash; auch ohne Kabel &mdash; Teilnehmer unterschiedlicher Router miteinander zuverbinden.
+
+Das Freifunk Wiki klärt VPN so:
+<blockquote>Ein Virtual Private Network [...] ist ein Computernetz, das zum Transport privater Daten ein öffentliches Netz (zum Beispiel das Internet) nutzt. Teilnehmer eines VPN können Daten wie in einem lokalen Netzwerk (LAN) austauschen. Die einzelnen Teilnehmer selbst müssen hierzu nicht direkt verbunden sein. Die Verbindung über das öffentliche Netz wird üblicherweise verschlüsselt. [...] Mit Hilfe eines VPN besteht die Möglichkeit getrennte weit auseinander gelegene Funknetze, zum Beispeil verschiedene Freifunk-Netze, über eine Datenleitung miteinander zu verbinden &ndash; <a href="http://wiki.freifunk.net/VPN">wiki.freifunk.net/VPN</a></blockquote></dd>
+</dl>
+</p>
 
     <script>
       $('[data-toggle="tooltip"]').tooltip({placement: 'top', html: true})
