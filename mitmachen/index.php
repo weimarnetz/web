a01f42febbb314496bb14be3356e932f182cbbb0
mr <mr@m18.uni-weimar.de>
2014-04-09 20:29:58 +0200

    und noch mehr typos gefixt - kommas nach dem abendbrot

--- mitmachen/index.php
+++ mitmachen/index.php
@@ -18,7 +18,7 @@
 				<img class="img-responsive"  src="../img/participate/participate_medium.png">
 			</div>
 			<div class="case-body">
-				<p>Stelle deinen Internetanschluss einfach und sicher für andere zur Verfügung. Ideal für Cafés oder Arbeitsräume. Auch für Touristen sind offene und kostenlose Hotspots praktisch.</p>
+				<p>Stelle Deinen Internetanschluss einfach und sicher für andere zur Verfügung. Ideal für Cafés oder Arbeitsräume. Auch für Touristen sind offene und kostenlose Hotspots praktisch.</p>
 			</div>
 		</div>
 	</div>
@@ -42,14 +42,14 @@
 				<img class="img-responsive"  src="../img/participate/participate_small.png">
 			</div>
 			<div class="case-body">
-				<p>Entdecke Freifunk und nutze angebotene Dienste im Netzwerk. Du kannst auch selber Dienste von zuhause aus betreiben. Z.B. einen Webserver oder ein Radio-Streaming.</p>
+				<p>Entdecke Freifunk und nutze angebotene Dienste im Netzwerk. Du kannst auch selbst Dienste von Zuhause aus betreiben. Beispielsweise einen Webserver oder ein Radio-Streaming.</p>
 			</div>
 		</div>
 	</div>
 </div>
 
 <h3>Wo kann ich mit machen?</h3>
-<p>Du kannst einen Router quasi überall aufbauen. Am besten gibt es vor Ort eine Möglichkeit den Router ans Internet anzuschließen oder es gibt bereits Freifunk-Nachbarn.</p>
+<p>Du kannst einen Router quasi überall aufbauen. Am Besten gibt es vor Ort eine Möglichkeit den Router ans Internet anzuschließen oder es gibt bereits Freifunk-Nachbarn.</p>
 <p>Sieh auf der Karte nach, ob bereits Knotenpunkte in deiner Nachbarschaft sind mit denen sich dein Router verbinden kann. Du kannst auch zusammen mit anderen einen Aufbau planen. Das <!--FIXME: Wiki-Seite vorher ueberarbeiten-->Weimarnetz-Treffen findet jeden Diestag im <a href="http://blog.maschinenraum.tk/contact/">Maschinenraum</a> statt. Wenn Du nicht zum Treffen kommen kannst, erreichst Du den Verein auch über die <a href="http://wireless.subsignal.org/index.php?title=Mailingliste">Mailingliste</a>, die <a href="mailto:kontakt@weimarnetz.de">Kontakt-E-Mail-Adresse</a>, oder auch über das Weimarnetz Sorgentelefon unter +49 3643 544304.</p>
 <div style="text-align: center">
 <iframe id="ifrm" src="http://map.weimarnetz.de/map.html#bbox=50.9484779952583,11.194210052490234,51.00651822828961,11.441402435302734" width="98%" scrolling="yes" marginwidth="0" marginheight="0" frameborder="0" style="height: 400px;">Weimarnetz Knoten Karte</iframe>
@@ -57,10 +57,10 @@
 
 <h3>Gibt es beim Router aufstellen etwas zu bachten?</h3>
 <dl>
-<dt>Die Teilname als Nutzer oder Knotenbetreiber setzt keine Registrierung, Anmeldung, Erlaubnis oder ähnliches vorraus!</dt>
+<dt>Die Teilname als Nutzer oder Knotenbetreiber setzt keine Registrierung, Anmeldung, Erlaubnis oder Ähnliches voraus!</dt>
 <dd>Das Netz ist frei und offen für jeden, wie im <a href="http://www.picopeer.net/PPA-de.html">Pico Peering Agreement</a> vereinbart.<br \>
 Jeder kann neue Router aufstellen. Wir empfehlen Einsteigern dennoch zum Treffen zu kommen, denn vor Ort und persönlich könnnen offene Fragen besser geklärt und nützliche Tipps ausgetauscht werden. Wenn Du möchtest kannst Du Dich auch <a href=https://lists.subsignal.org/mailman/listinfo/wireless-discuss">auf der Mailingliste anmelden</a> um über Treffen und Neuigkeiten informiert zu werden.</dd><br \>
-<dt>Wie ist das rechtlich, wenn ich mein Internetanschluss anderen zur Verfügung stelle?</dt>
+<dt>Wie ist das rechtlich, wenn ich meinen Internetanschluss anderen zur Verfügung stelle?</dt>
 <dd>Um Internet-Einspeisende vor möglichen Abmahnungen zu schützen und von der sogenannten Störerhaftung zu entlasten betreibt der Weimarnetz e.V. einen VPN-Server in Berlin über den alle WLAN-Teilnehmerinnen getunnelt werden. Dadurch können Nutzerinnen des Weimarnetz nicht direkt einem Internetanschluss zugeordnet werden.</dd>
 </dl>
 
@@ -77,11 +77,11 @@ Wie und woher bekomme ich einen Router?</h3>
 	<li>Ubiquiti <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m2-2-4ghz-a651912.html" target="_blank">NanoStation M2</a> &amp; <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m2-loco-a651904.html" target="_blank">Loco</a>, <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m5-5ghz-a651909.html" target="_blank">NanoStation M5</a>  &amp; <a href="http://www.heise.de/preisvergleich/ubiquiti-nanostation-m5-loco-a651908.html" target="_blank">Loco</a>, oder <a href="http://www.heise.de/preisvergleich/ubiquiti-nanobridge-m5-25db-i-nb-5g25-a651918.html" target="_blank">NanoBridge M5</a> für Richtfunk bis zu 5 km (50 bis 80 &euro;)</li>
 	</ul>
 </p>
-<p>Es ist auch möglich Dienstags beim Treffen einen Router (TL-WR841) zu bekommen (sofern welche auf Lager sind) oder sich an Sammelbestellungen zu beteiligen. Am besten schreibst Du uns vorher eine <a href="mailto:kontakt@weimarnetz.de">E-Mail</a> oder hinterlässt eine Nachricht auf dem <a href="/contact">Anrufbeantworter</a>.
+<p>Es ist auch möglich dienstags beim Treffen einen Router (TL-WR841) zu bekommen (sofern welche auf Lager sind) oder sich an Sammelbestellungen zu beteiligen. Am Besten schreibst Du uns vorher eine <a href="mailto:kontakt@weimarnetz.de">E-Mail</a> oder hinterlässt eine Nachricht auf dem <a href="/contact">Anrufbeantworter</a>.
 </p>
 
 <h3>Wie kommt die Weimarnetz-Firmware auf den Router?</h3>
-<p>Im Wiki findest Du eine <a href="http://wireless.subsignal.org/index.php?title=Firmware-Installation">Anleitung</a> wie Du mit dem <a href="http://meshkit.weimarnetz.de/">Meshkit</a> die Firmware vorbereitest und dann auf dem Router <a href="http://wireless.subsignal.org/index.php?title=Firmware-Installation">installierst</a>. Wenn Du Hilfe benötigst oder Dir etwas unklar ist, schreib uns auf der Mailingliste oder komm zum wöchtentlichen Treffen in den Maschinenraum, Marienstraße 18.
+<p>Im Wiki findest Du eine <a href="http://wireless.subsignal.org/index.php?title=Firmware-Installation">Anleitung</a> wie Du mit dem <a href="http://meshkit.weimarnetz.de/">Meshkit</a> die Firmware vorbereitest und dann auf dem Router <a href="http://wireless.subsignal.org/index.php?title=Firmware-Installation">installierst</a>. Wenn Du Hilfe benötigst oder Dir etwas unklar ist, schreib uns auf der Mailingliste oder komm zum wöchentlichen Treffen in den Maschinenraum, Marienstraße 18.
 </p>
 
 <h3>Ich möchte verstehen wie die Firmware funktioniert und Änderungen vornehmen!</h3>
@@ -93,8 +93,8 @@ Wie und woher bekomme ich einen Router?</h3>
 <ul>
 <li><strong>Werbung machen!</strong> Erzähle deinen Freunden und Freundinnen, Arbeitskollegen und Kolleginnen, deiner Lieblingskneipe und anderen von der Freifunk-Idee und bring sie mit uns in Kontakt. Wir helfen euch gerne beim Aufstellen und Betreiben von Knotenpunkten.</li>
 <li><strong>Spenden!</strong> Unterstütze den Verein mit einmaligen oder monatlichen <a href="/spenden/">Spenden</a>. Auf <a href="http://www.betterplace.org/de/projects/14895-weimarnetz-e-v-freies-wlan-in-weimar">Betterplace</a> kannst Du auch für konkrete Projekte spenden. Als monatlicher Beitrag haben sich 5,00 &euro; etabliert. Diese werden z.B. für den Betrieb des Servers in Berlin bei <a href="https://www.in-berlin.de/verein/">IN-Berlin</a> und das Anschaffen von Routern, Kabeln, PoE-Adaptern usw. verwendet.</li>
-<li><strong>Inhalte auf der Website und im Wiki verbessern und aktuallisieren!</strong> Wir sind meistens nicht die Kommunikationsexperten und freuen uns über Hilfe bei der Aussenwerbung und -Kommunikation, sowie bei besseren Erklärungen der technischen Zusammenhänge.</li>
-<li><strong>Sag uns, was du nicht verstehst!</strong> Wir wollen Verständnis-Barrieren abbauen. Leider sind wir was das angeht oft Betriebsblind. Hilf uns, überkomplizierte Erklärungen zu erkennen und zu vereinfachen.</li>
+<li><strong>Inhalte auf der Website und im Wiki verbessern und aktualisieren!</strong> Wir sind meistens nicht die Kommunikationsexperten und freuen uns über Hilfe bei der Außenwerbung und -Kommunikation, sowie bei besseren Erklärungen der technischen Zusammenhänge.</li>
+<li><strong>Sag uns, was du nicht verstehst!</strong> Wir wollen Verständnisbarrieren abbauen. Leider sind wir was das angeht oft betriebsblind. Hilf uns, überkomplizierte Erklärungen zu erkennen und zu vereinfachen.</li>
 </ul>
 </p>
 
@@ -104,7 +104,7 @@ Wie und woher bekomme ich einen Router?</h3>
 <dd>Wenn ein Freifunk-Router direkt oder indirekt über andere das Internet erreichen kann, wird er automatisch Teil des <em>Virtual Private Network</em>.<br \>
 Jeder Router der sich mit anderen Routern über Funk verbinden kann, bildet zusammen mit diesen ein Maschennetz (engl. Mesh-Network). Das Mesh-Net beschreibt die besondere Eigenschaft des Netzwerks &mdash; auch ohne Kabel &mdash; Teilnehmer unterschiedlicher Router miteinander zuverbinden.<br \>
 Das Freifunk Wiki erklärt VPN:
-<blockquote>Ein Virtual Private Network [...] ist ein Computernetz, das zum Transport privater Daten ein öffentliches Netz (zum Beispiel das Internet) nutzt. Teilnehmer eines VPN können Daten wie in einem lokalen Netzwerk (LAN) austauschen. Die einzelnen Teilnehmer selbst müssen hierzu nicht direkt verbunden sein. Die Verbindung über das öffentliche Netz wird üblicherweise verschlüsselt. [...] Mit Hilfe eines VPN besteht die Möglichkeit getrennte weit auseinander gelegene Funknetze, zum Beispeil verschiedene Freifunk-Netze, über eine Datenleitung miteinander zu verbinden &ndash; <a href="http://wiki.freifunk.net/VPN">wiki.freifunk.net/VPN</a></blockquote></dd>
+<blockquote>Ein Virtual Private Network [...] ist ein Computernetz, das zum Transport privater Daten ein öffentliches Netz (zum Beispiel das Internet) nutzt. Teilnehmer eines VPN können Daten wie in einem lokalen Netzwerk (LAN) austauschen. Die einzelnen Teilnehmer selbst müssen hierzu nicht direkt verbunden sein. Die Verbindung über das öffentliche Netz wird üblicherweise verschlüsselt. [...] Mit Hilfe eines VPN besteht die Möglichkeit getrennte weit auseinander gelegene Funknetze, zum Beispiel verschiedene Freifunk-Netze, über eine Datenleitung miteinander zu verbinden &ndash; <a href="http://wiki.freifunk.net/VPN">wiki.freifunk.net/VPN</a></blockquote></dd>
 </dl>
 </p>
 
