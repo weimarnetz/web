156d47d118e72b013b86032de4232548d8c3b84f
mr <mr@m18.uni-weimar.de>
2014-04-17 21:19:55 +0200

    added links

--- about/index.php
+++ about/index.php
@@ -47,7 +47,7 @@ um Erfahrungen und Ideen auszutauschen und den Aufbau und
 die Entwicklung des gemeinsamen Netzes zu koordinieren.
 Wir sehen uns als lokale Umsetzung der freifunk.net-Initiative.</p>
 <p>Das Weimarnetz wird vom Weimarnetz e.V. gefördert, welcher sich duch Spenden finanziert.<br>
-Weitere Förderer sind das free software and infrastructure tech collective |||subsignal.org, die Initiative F.E.I.N.T.org, bittorf wireless )) und der StuKo der Bauhaus-Universität Weimar.</p>
+Weitere Förderer sind das <em>free software and infrastructure tech collective</em> <a href="http://subsignal.org/">||| subsignal.org</a>, die Initiative F.E.I.N.T.org, <a href="http://www.bittorf-wireless.de/">bittorf wireless ))</a> und der <a href="http://m18.uni-weimar.de/stuko">StuKo der Bauhaus-Universität Weimar</a> durch seine studentische Initiative <a href="http://blog.maschinenraum.tk/">Maschinenraum</a>.</p>
 <p>Freifunk-Netze sind Selbstmach-Netze. Für den Aufbau nutzen Teilnehmer auf ihren WLAN-Routern, eine spezielle Linuxdistribution, die Freifunk-Firmware. Lokale Communities stellen die auf eigene Bedürfnisse angepasste Software dann auf ihren Websites zur Verfügung. In Dörfern und Städten gibt es immer mehr Freifunk-Gruppen und Zusammenkünfte, wo sich Interessierte treffen. Die Freifunk-Community ist Teil einer globalen Bewegung für freie Infrastrukturen. Unsere Vision ist die Demokratisierung der Kommunikationsmedien durch freie Netzwerke. Die praktische Umsetzung dieser Idee nehmen Freifunk-Communities in der ganzen Welt in Angriff.</p> 
 
     	
