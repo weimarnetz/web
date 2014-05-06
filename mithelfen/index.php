779ec41f68fd082659cd31bcabf0bc60cd2167b4
ed <bernd@kr217.de>
2014-05-06 14:48:38 +0200

    copied andis text from pad to html #1

---
+++ mithelfen/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+  <title>Weimarnetz - Mithelfen</title>
+  <?php include("../inc/_head.inc.php") ?>
+</head>
+
+<body>
+
+  <?php include("../inc/navbar.inc.php") ?>
+
+  <div class="container">
+
+<h3>Wie kann ich bei der Weiterentwicklung des Weimarnetz mithelfen?</h3>
+<p>Es gibt im Weimarnetz viele Möglichkeiten &mdash; neben dem Aufstellen eines Routers &mdash; sich einzubringen und an spanndenden Projekten mitzuarbeiten und so das Weimarnetz zu verbessern und lebendig zu gestalten. Einige mögliche Einstiegspunkte wollen wir im folgenden vorstellen.</p>
+<p>Grundsätzlich gilt: Wir arbeiten im Team an Projekten und Problemen, und helfen uns gegenseitig. Aufgaben werden auf mehrere Schultern verteilt um zu vermeiden, dass aufgebautes Wissen verloren geht, falls jemand Weimar verlässt oder aus anderen Gründen nicht mehr mitarbeiten kann. Durch regelmäßige Treffen halten wir uns gegenseitig auf dem Laufenden. Auch hier soll sich die in Freifunk-Communities immer wieder propagierte Dezentralität widerspiegeln.</p>
+<p>Die Ergebnisse unserer Arbeit stellen wir an öffentlich zugänglichen Orten (z.B. <a href="https://github.com/weimarnetz/">Github</a> oder als Download auf dieser Website) allen zur Verfügung und statten sie mit einer OpenSource-Lizenz aus. Regelmäßig tauschen wir uns mit anderen Freifunk-Communities aus, um gemeinsam Ideen zu erarbeiten, Resourcen effizient zu nutzen und sich unter Umständen doppelte Arbeit zu ersparen.</p>
+<p>Zur gemeinsamen Koordination der anfallenden Aufgaben verwenden wir <a href="https://www.trello.com/weimarnetz">Trello</a> und sehen so transparent, was gerade ansteht und wo Hilfe benötigt wird.</p>
+
+
+<hr>
+
+<h4>Öffentlichkeitsarbeit</h4>
+<p>Ein wichtiger Bestandteil zur Verbreitung unserer Idee und zur Erweiterung des Netzes ist die Öffentlichkeitsarbeit. Diese hilft uns Materialien zu erstellen mit denen wir um neue Mitstreiter werben können, Anfragen zu bearbeiten oder Kontakt zu unseren Kooperationspartnern aufrecht zu erhalten.</p>
+<h5>Das sind typische Arbeitsschwerpunkte in diesem Bereich:</h5>
+<p>
+<ul>
+<li>Reaktion auf Anfragen über konktakt@weimarnetz.de</li>
+<li>Entwurf und Pflege eines einheitlichen Designs für Flyer, Poster, Aufkleber, Postkarten und Websites</li>
+<li>Schreiben von (Werbe-)Texten</li>
+<li>Spendenaufrufe</li>
+<li>Kontakt zu Kooperationspartnern (Stadt Weimar, Unternehmen, Presse)</li>
+<li>Werbung für Events</li>
+</ul>
+</p>
+
+<!--Dir liegt es, Werbung zu machen, Designs zu erstellen oder mit Menschen über unser Projekt zu reden? Dann engagier dich in der Öffentlichkeitsarbeit.-->
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: gering</li>
+<li>Kommunikation: hoch</li>
+<li>Design: hoch</li>
+<li>Programmierung: nicht notwendig</li>
+</ul>
+</p>
+
+<hr>
+
+
+<h4>Verwaltung/Verein</h4>
+<p>Unser Verein weimarnetz e.V. ist kein Mitgliederverein. Er dient dazu, das Freifunkprojekt in Weimar (weimarnetz) zu unterstützen und zu fördern, Veranstaltungen durchzuführen oder Bildungsarbeit zu leisten. Niemand muss Mitglied im Verein werden um am Weimarnetz teilzunehmen oder an unseren Projekten mitzuarbeiten.</p>
+<p>Trotzdem macht auch ein solcher Verein Arbeit: Spenden wollen verwaltet und bestätigt werden, wir organisieren Mitgliederversammlungen, Rechnung, Kontoauszüge und Rechnungen müssen wir abheften, alle 3 Jahre erwartet das Finanzamt eine Steuererklärung von uns.</p>
+<p>Wenn dir Verwaltungstätigkeiten liegen oder dir Steuererklärungen Freude bereiten bist du willkommen uns hier zu helfen.</p>
+
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: nicht notwendig</li>
+<li>Kommunikation: hoch</li>
+<li>Design: nicht notwendig</li>
+<li>Programmierung: nicht notwendig</li>
+</ul>
+
+<hr>
+
+
+<h4>Website</h4>
+<p>Eine erste Anlaufstelle für Neugierige und Interessenten ist unsere Website www.weimarnetz.de. Die Inhalte sollten wir so aktuell wie möglich halten, um uns und andere nicht zu verwirren. Deshalb sind dort immer wieder Anpassungen erforderlich. Wir möchten uns auch regelmäßig mit unbedarfteren Nutzern treffen, um Schwachstellen in der Benutzerführung auf der Seite oder zu kompliziert aufgebaute Texte zu finden.</p>
+<p>Eng verbunden mit der Website ist unser Wiki unter wireless.subsignal.org. Das Wiki verwenden wir, um bebilderte Anleitungen und Texte für Spezialisten abzulegen oder Dokumentationen zu schreiben, deren Inhalte immer wieder angepasst werden müssen. Diese Inhalte lassen sich leicht ändern und anpassen, bieten aber nicht die gleichen Designmöglichkeiten wie reine Websites. Außerdem neigen Wikis ständig zu einer gewissen Unordnung, die es schwer macht, schnell einen Überblick zu finden.</p>
+<p>Die Website pflegen wir in unserem Repository auf github.com: https://github.com/weimarnetz/web<p>
+<p>Unter https://github.com/weimarnetz/wiki findet man Einstellungen und wichtige Dokumente unseres Wikis.</p>
+<p>
+
+<h5>Typische Tätigkeiten in diesem Bereich sind</h5>
+<ul>
+<li>Pflege von Inhalten in HTML-Seiten</li>
+<li>Anpassungen des Design in Zusammenarbeit mit Öffentlichkeitsarbeit</li>
+<li>Anpassung von dynamischen Inhalten bei Änderungen</li>
+<li>Übertragung des Designs auf andere Dienste (Wiki, Meshkit, Registrator, DynDNS, ...)</li>
+</ul>
+</p>
+
+<!--Hast du Spaß an der Arbeit mit HTML, JavaScript, PHP oder anderen, neuen Webtechniken, dann hilf uns bei unserer Website.-->
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: nicht notwendig</li>
+<li>Kommunikation: mittel</li>
+<li>Design: mittel</li>
+<li>Programmierung: hoch</li>
+</ul>
+</p>
+
+<hr>
+
+
+<h4>Installationen</h4>
+<p>Eine nachhaltige Verbesserung des Netzes können wir durch gezielte Installationen von Routern zwischen Gebäuden oder an markanten Punkten erreichen. Diese Orte zeichnen sich durch gerichtete Funkstrecken aus und können mehrere Himmelsrichtungen abdecken und sollten gut geplant und mit den Funknachbarn abgesprochen sein.</p>
+
+<h5>Aufgaben</h5>
+<p>
+<ul>
+<li>Planung von Installationen</li>
+<li>Absprache mit Hauseigentümern und Ansprechpartnern vor Ort</li>
+<li>Vorbereitung von Gestattungsverträgen</li>
+<li>Installation der Geräte</li>
+</ul>
+</p>
+
+<!--Dir gefallen handfeste Ergebnisse und Höhenangst ist ein Fremdwort - dann bist du hier richtig.-->
+
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: hoch</li>
+<li>Kommunikation: mittel</li>
+<li>Design: nicht notwendig</li>
+<li>Programmierung: nicht notwendig</li>
+</ul>
+</p>
+
+<hr>
+
+
+<h4>Firmware</h4>
+<p>Ein Herzstück für unser Netz und gleichzeitig einer unserer größten "Brocken" ist unsere Firmware, mit der wir unsere Geräte im Weimarnetz überhaupt zusammenbringen. Sie basiert auf OpenWRT und wir versuchen hier sehr stark auch Komponenten anderer Freifunkcommunities wiederzuverwenden. Neue Anforderungen, Weiterentwicklungen im OpenWRT oder durch neue, leistungsfähigere Hardware, die auf den Markt kommt erfordern ständige Anpassungen und vor allem regelmäßiges Testen. Die Firmwareentwicklung ist auch eng verzahnt mit den Nachbarprojekten Registrator und Meshkit. Änderungen an Firmware oder in der Administration können jeweils auch Auswirkungen aufeinander haben.</p>
+<p>Die Erweiterungen von OpenWRT für das Weimarnetz legen wir im Github unter https://github.com/weimarnetz/weimarnetz und https://github.com/weimarnetz/openwrt-packages ab.</p>
+
+<h5>In der Firmwareentwicklung ist folgendes zu tun</h5>
+<p>
+<ul>
+<li>Analyse und Umsetzung von Anforderungen an die Firmware</li>
+<li>Anpassung der Firmware an neue Hardware</li>
+<li>Tests der Firmware, Aufbau eines standardisierten Testprozesses</li>
+<li>Bauen von OpenWRT-Paketen</li>
+<li>Erstellen und Pflege eines Buildprozesses, z.B. um auch für das Meshkit reproduzierbar eine Buildumgebung zur Verfügung zu stellen</li>
+<li>Analyse von Fehlern und Problemen</li>
+</ul>
+</p>
+
+<!--Du sprichst fließend shell-Code, träumst in Lua oder spielst gern mit neuer Hardware und entwickelst gern nah an der Hardware, dann kann die Firmwareentwicklung ein spannendendes Feld für dich sein.-->
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: hoch</li>
+<li>Kommunikation: gering</li>
+<li>Design: gering</li>
+<li>Programmierung: hoch</li>
+</ul>
+</p>
+
+<hr>
+
+
+<h4>Registrator</h4>
+<p>Mit dem Registrator verwalten wir die sogenannten Knotennummern im Weimarnetz. Die Knotennummer ist eine eindeutig Nummer, die einen Router identifiziert und die Grundlage für seine Netzwerkkonfiguration bildet.</p>
+<p>Der Quellcode des Registrators ist unter https://github.com/weimarnetz/registrator auffindbar.</p>
+<h5>Tätigkeiten</h5>
+<ul>
+<li>Datenpflege und Zusammenspiel mit Firmware optimieren</li>
+<li>Integration neuer Anforderungen</li>
+</li>Fehleranalyse</li>
+</ul>
+</p>
+
+<!--Mit node.js und Erfahrung im Betrieb von Diensten kannst du dich beim Registrator einbringen.-->
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: gering</li>
+<li>Kommunikation: gering</li>
+<li>Design: mittel</li>
+<li>Programmierung: hoch</li>
+</ul>
+</p>
+
+<hr>
+
+
+<h4>Meshkit</h4>
+<p>Das Meshkit gibt uns die Möglichkeit, individuelle und fertig konfigurierte Firmwareimages für Router auszuliefern. Mit dem Meshkit konnten wir die Komplexität und Anzahl der Schritte vom gekaufen Router zum einsatzbereiten Weimarnetzgerät extrem verringern und für Nutzer einen inzwischen recht einfachen Weg zur Teilnahme an Netz bereiten.</p>
+<p>Unsere angepasste Version des Meshkit kann man unter https://github.com/weimarnetz/meshkit finden.</p>
+
+<h5>Schwerpunkte</h5>
+<p>
+<ul>
+<li>Integration neuer Firmwareversionen oder Erweiterungen der Firmware</li>
+<li>Unterstützung neuer Hardware</li>
+<li>Übernahme von Designänderungen an der Website</li>
+<li>Testen</li>
+<li>Absprache mit dem Entwickler (Soma aus Augsburg) und Integration von gegenseitigen Änderungen</li>
+<li>Fehleranalyse und Problembehebung</li>
+</ul>
+</p>
+
+<!--Python ist für dich nicht nur eine Schlange? Dann passt du bestens zum Meshkit!-->
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: gering</li>
+<li>Kommunikation: gering</li>
+<li>Design: mittel</li>
+<li>Programmierung: hoch</li>
+</ul>
+</p>
+
+<hr>
+
+
+<h4>Administration</h4>
+<p>Der Verein betreibt einen eigenen Rootserver, der in Berlin steht und betreibt darauf und bei befreundeten Communities insgesamt 3 virtuelle Server. Darauf laufen unter anderem diese Website, die VPN-Endpunkte für unsere Router, einen Jabberserver und einen DynDNS-Dienst. Die Dienste und Betriebssysteme auf den Servern müssen regelmäßig aktuell gehalten und hin und wieder an neue Gegebenheiten abgepasst werden. Ein Monitoringsystem informiert uns über Störungen an Hard- und Software.</p>
+
+<h5>Hauptsächlich geht es um</h5>
+<p>
+<ul>
+<li>Administration von Linux-Betriebssystemen</li>
+<li>regelmäßige Aktualisierung</li>
+<li>Überwachung der Hardware</li>
+<li>Installation und Betrieb von Diensten</li>
+</ul>
+</p>
+<!--Für alle, die sich gern im Hintergrund aufhalten ist die Administration der Server ein idealer Job.-->
+<h5>Fähigkeiten</h5>
+<p>
+<ul>
+<li>Hardware: hoch</li>
+<li>Kommunikation: gering</li>
+<li>Design: nicht notwendig</li>
+<li>Programmierung: gering</li>
+</ul>
+</p>
+
+
+    <script>
+      $('[data-toggle="tooltip"]').tooltip({placement: 'top', html: true})
+      $('[data-toggle="popover"]').popover({placement: 'top', html: true})
+    </script>
+
+    <?php include("../inc/footer.inc.php") ?>
+
+  </div> <!-- ende container -->    
+
+  <?php include("../inc/_foot.inc.php") ?>
+
+
+</body>
+</html>
