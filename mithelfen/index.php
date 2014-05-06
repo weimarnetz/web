144110d669bbeecc58ec6a4e7357ea806fa5bbcc
ed <bernd@kr217.de>
2014-05-06 15:44:18 +0200

    added links and simple typo fixes

--- mithelfen/index.php
+++ mithelfen/index.php
@@ -65,7 +65,7 @@
 
 
 <h4 id="Verwaltung-Verein">Verwaltung/Verein</h4>
-<p>Unser Verein weimarnetz e.V. ist kein Mitgliederverein. Er dient dazu, das Freifunkprojekt in Weimar (weimarnetz) zu unterstützen und zu fördern, Veranstaltungen durchzuführen oder Bildungsarbeit zu leisten. Niemand muss Mitglied im Verein werden um am Weimarnetz teilzunehmen oder an unseren Projekten mitzuarbeiten.</p>
+<p>Unser Verein weimarnetz e.V. ist kein Mitgliederverein. Er dient dazu das Freifunkprojekt in Weimar (weimarnetz) zu unterstützen und zu fördern, Veranstaltungen durchzuführen oder Bildungsarbeit zu leisten. Niemand muss Mitglied im Verein werden um am Weimarnetz teilzunehmen oder an unseren Projekten mitzuarbeiten.</p>
 <p>Trotzdem macht auch ein solcher Verein Arbeit: Spenden wollen verwaltet und bestätigt werden, wir organisieren Mitgliederversammlungen, Rechnung, Kontoauszüge und Rechnungen müssen wir abheften, alle 3 Jahre erwartet das Finanzamt eine Steuererklärung von uns.</p>
 <p>Wenn dir Verwaltungstätigkeiten liegen oder dir Steuererklärungen Freude bereiten bist du willkommen uns hier zu helfen.</p>
 
@@ -82,10 +82,9 @@
 
 
 <h4 id="Website">Website</h4>
-<p>Eine erste Anlaufstelle für Neugierige und Interessenten ist unsere Website www.weimarnetz.de. Die Inhalte sollten wir so aktuell wie möglich halten, um uns und andere nicht zu verwirren. Deshalb sind dort immer wieder Anpassungen erforderlich. Wir möchten uns auch regelmäßig mit unbedarfteren Nutzern treffen, um Schwachstellen in der Benutzerführung auf der Seite oder zu kompliziert aufgebaute Texte zu finden.</p>
-<p>Eng verbunden mit der Website ist unser Wiki unter wireless.subsignal.org. Das Wiki verwenden wir, um bebilderte Anleitungen und Texte für Spezialisten abzulegen oder Dokumentationen zu schreiben, deren Inhalte immer wieder angepasst werden müssen. Diese Inhalte lassen sich leicht ändern und anpassen, bieten aber nicht die gleichen Designmöglichkeiten wie reine Websites. Außerdem neigen Wikis ständig zu einer gewissen Unordnung, die es schwer macht, schnell einen Überblick zu finden.</p>
-<p>Die Website pflegen wir in unserem Repository auf github.com: https://github.com/weimarnetz/web<p>
-<p>Unter https://github.com/weimarnetz/wiki findet man Einstellungen und wichtige Dokumente unseres Wikis.</p>
+<p>Eine erste Anlaufstelle für Neugierige und Interessenten ist unsere Website <a href="http://www.weimarnetz.de">www.weimarnetz.de</a>. Die Inhalte sollten wir so aktuell wie möglich halten, um uns und andere nicht zu verwirren. Deshalb sind dort immer wieder Anpassungen erforderlich. Wir möchten uns auch regelmäßig mit unbedarfteren Nutzern treffen, um Schwachstellen in der Benutzerführung auf der Seite oder zu kompliziert aufgebaute Texte zu finden.</p>
+<p>Eng verbunden mit der Website ist unser Wiki unter <a href="https://wireless.subsignal.org/index.php?title=Hauptseite">wireless.subsignal.org</a>. Das Wiki verwenden wir, um bebilderte Anleitungen und Texte für Spezialisten abzulegen oder Dokumentationen zu schreiben, deren Inhalte immer wieder angepasst werden müssen. Diese Inhalte lassen sich leicht ändern und anpassen, bieten aber nicht die gleichen Designmöglichkeiten wie reine Websites. Außerdem neigen Wikis ständig zu einer gewissen Unordnung, die es schwer macht, schnell einen Überblick zu finden.</p>
+<p>Die Website pflegen wir in unserem Repository auf <a href="https://github.com/weimarnetz/web">https://github.com/weimarnetz/web</a>, und unter <a href="https://github.com/weimarnetz/wiki">https://github.com/weimarnetz/wiki</a> findet man Einstellungen und wichtige Dokumente unseres Wikis.</p>
 <p>
 
 <h5>Typische Tätigkeiten in diesem Bereich sind</h5>
@@ -140,8 +139,8 @@
 
 
 <h4 id="Firmware">Firmware</h4>
-<p>Ein Herzstück für unser Netz und gleichzeitig einer unserer größten "Brocken" ist unsere Firmware, mit der wir unsere Geräte im Weimarnetz überhaupt zusammenbringen. Sie basiert auf OpenWRT und wir versuchen hier sehr stark auch Komponenten anderer Freifunkcommunities wiederzuverwenden. Neue Anforderungen, Weiterentwicklungen im OpenWRT oder durch neue, leistungsfähigere Hardware, die auf den Markt kommt erfordern ständige Anpassungen und vor allem regelmäßiges Testen. Die Firmwareentwicklung ist auch eng verzahnt mit den Nachbarprojekten Registrator und Meshkit. Änderungen an Firmware oder in der Administration können jeweils auch Auswirkungen aufeinander haben.</p>
-<p>Die Erweiterungen von OpenWRT für das Weimarnetz legen wir im Github unter https://github.com/weimarnetz/weimarnetz und https://github.com/weimarnetz/openwrt-packages ab.</p>
+<p>Das Herzstück für unser Netz und gleichzeitig einer unserer größten &quot;Brocken&quot; ist unsere Firmware, mit der wir unsere Geräte im Weimarnetz überhaupt zusammenbringen. Sie basiert auf OpenWRT und wir versuchen hier sehr stark auch Komponenten anderer Freifunk-Communities wiederzuverwenden. Neue Anforderungen, Weiterentwicklungen im OpenWRT oder durch neue, leistungsfähigere Hardware, die auf den Markt kommt erfordern ständige Anpassungen und vor allem regelmäßiges Testen. Die Firmwareentwicklung ist auch eng verzahnt mit den Nachbarprojekten Registrator und Meshkit. Änderungen an Firmware oder in der Administration können jeweils auch Auswirkungen aufeinander haben.</p>
+<p>Die Erweiterungen von OpenWRT für das Weimarnetz legen wir im Github unter <a href="https://github.com/weimarnetz/weimarnetz">https://github.com/weimarnetz/weimarnetz</a> und <a href="https://github.com/weimarnetz/openwrt-packages">https://github.com/weimarnetz/openwrt-packages</a> ab.</p>
 
 <h5>In der Firmwareentwicklung ist folgendes zu tun</h5>
 <p>
@@ -171,12 +170,13 @@
 
 <h4 id="Registrator">Registrator</h4>
 <p>Mit dem Registrator verwalten wir die sogenannten Knotennummern im Weimarnetz. Die Knotennummer ist eine eindeutig Nummer, die einen Router identifiziert und die Grundlage für seine Netzwerkkonfiguration bildet.</p>
-<p>Der Quellcode des Registrators ist unter https://github.com/weimarnetz/registrator auffindbar.</p>
+<p>Der Quellcode des Registrators ist unter <a href="https://github.com/weimarnetz/registrator">https://github.com/weimarnetz/registrator</a> zu finden.</p>
 <h5>Tätigkeiten</h5>
+<p>
 <ul>
 <li>Datenpflege und Zusammenspiel mit Firmware optimieren</li>
 <li>Integration neuer Anforderungen</li>
-</li>Fehleranalyse</li>
+<li>Fehleranalyse</li>
 </ul>
 </p>
 
@@ -196,7 +196,7 @@
 
 <h4 id="Meshkit">Meshkit</h4>
 <p>Das Meshkit gibt uns die Möglichkeit, individuelle und fertig konfigurierte Firmwareimages für Router auszuliefern. Mit dem Meshkit konnten wir die Komplexität und Anzahl der Schritte vom gekaufen Router zum einsatzbereiten Weimarnetzgerät extrem verringern und für Nutzer einen inzwischen recht einfachen Weg zur Teilnahme an Netz bereiten.</p>
-<p>Unsere angepasste Version des Meshkit kann man unter https://github.com/weimarnetz/meshkit finden.</p>
+<p>Unsere angepasste Version des Meshkit kann man unter <a href="https://github.com/weimarnetz/meshkit">https://github.com/weimarnetz/meshkit</a> finden.</p>
 
 <h5>Schwerpunkte</h5>
 <p>
@@ -225,7 +225,7 @@
 
 
 <h4 id="Administration">Administration</h4>
-<p>Der Verein betreibt einen eigenen Rootserver, der in Berlin steht und betreibt darauf und bei befreundeten Communities insgesamt 3 virtuelle Server. Darauf laufen unter anderem diese Website, die VPN-Endpunkte für unsere Router, einen Jabberserver und einen DynDNS-Dienst. Die Dienste und Betriebssysteme auf den Servern müssen regelmäßig aktuell gehalten und hin und wieder an neue Gegebenheiten abgepasst werden. Ein Monitoringsystem informiert uns über Störungen an Hard- und Software.</p>
+<p>Der Verein betreibt einen eigenen Rootserver, der in Berlin steht und betreibt darauf und bei befreundeten Communities insgesamt 3 virtuelle Server. Darauf laufen unter anderem diese Website, die VPN-Endpunkte für unsere Router, ein Jabber-Server und ein DynDNS-Dienst. Die Dienste und Betriebssysteme auf den Servern müssen regelmäßig aktuell gehalten und hin und wieder an neue Gegebenheiten angepasst werden. Ein Monitoringsystem informiert uns über Störungen an Hard- und Software.</p>
 
 <h5>Hauptsächlich geht es um</h5>
 <p>
