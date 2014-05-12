071fdf91aaf6d424a08b253ca4206229a370088f
ed <bernd@kr217.de>
2014-05-12 17:04:45 +0200

    hacked in-site navigation

--- mithelfen/index.php
+++ mithelfen/index.php
@@ -19,12 +19,11 @@
 
 <hr>
 
-<!-- #BROKEN! - #FIXME: CSS-Voodoo 
 <p>
 <ul>
 <li><a href="#Oeffentlichkeitsarbeit">Öffentlichkeitsarbeit</a></li>
 <li><a href="#Verwaltung-Verein">Verwaltung/Verein</a></li>
-<li><a href="#Webseite">Website</a></li>
+<li><a href="#Webseite">Webseite</a></li>
 <li><a href="#Installation">Installationen</a></li>
 <li><a href="#Firmware">Firmware</a></li>
 <li><a href="#Registrator">Registrator</a></li>
@@ -33,10 +32,10 @@
 </ul>
 </p>
 
-<hr>
--->
 
-<h4 id="Oeffentlichkeitsarbeit">Öffentlichkeitsarbeit</h4>
+<p id="Oeffentlichkeitsarbeit">&nbsp;</p>
+<hr>
+<h4>Öffentlichkeitsarbeit</h4>
 <p>Ein wichtiger Bestandteil zur Verbreitung unserer Idee und zur Erweiterung des Netzes ist die Öffentlichkeitsarbeit. Diese hilft uns Materialien zu erstellen mit denen wir um neue Mitstreiter werben können, Anfragen zu bearbeiten oder Kontakt zu unseren Kooperationspartnern aufrecht zu erhalten.</p>
 <p>Dir liegt es, Werbung zu machen, Designs zu erstellen oder mit Menschen über unser Projekt zu reden? Dann engagier dich in der Öffentlichkeitsarbeit.</p>
 
@@ -62,12 +61,12 @@
 </ul>
 </p>
 
+
+<p id="Verwaltung-Verein">&nbsp;</p>
 <hr>
-
-
-<h4 id="Verwaltung-Verein">Verwaltung/Verein</h4>
-<p>Unser Verein weimarnetz e.V. ist kein Mitgliederverein. Er dient dazu das Freifunkprojekt in Weimar (weimarnetz) zu unterstützen und zu fördern, Veranstaltungen durchzuführen oder Bildungsarbeit zu leisten. Niemand muss Mitglied im Verein werden um am Weimarnetz teilzunehmen oder an unseren Projekten mitzuarbeiten.</p>
-<p>Trotzdem macht auch ein solcher Verein Arbeit: Spenden wollen verwaltet und bestätigt werden, wir organisieren Mitgliederversammlungen, Rechnung, Kontoauszüge und Rechnungen müssen wir abheften, alle 3 Jahre erwartet das Finanzamt eine Steuererklärung von uns.</p>
+<h4>Verwaltung/Verein</h4>
+<p>Unser Verein weimarnetz e.V. ist kein Mitgliederverein. Er dient dazu das Freifunkprojekt in Weimar (Weimarnetz) zu unterstützen und zu fördern, Veranstaltungen durchzuführen oder Bildungsarbeit zu leisten. Niemand muss Mitglied im Verein werden um am Weimarnetz teilzunehmen oder an unseren Projekten mitzuarbeiten.</p>
+<p>Trotzdem macht auch ein solcher Verein Arbeit: Spenden wollen verwaltet und bestätigt werden, wir organisieren Mitgliederversammlungen, Kontoauszüge und Rechnungen müssen wir abheften, alle 3 Jahre erwartet das Finanzamt eine Steuererklärung von uns.</p>
 <p>Wenn dir Verwaltungstätigkeiten liegen oder dir Steuererklärungen Freude bereiten bist du willkommen uns hier zu helfen.</p>
 
 <h5>Fähigkeiten</h5>
@@ -79,10 +78,10 @@
 <li>Programmierung: &mdash;</li>
 </ul>
 
+
+<p id="Webseite">&nbsp;</p>
 <hr>
-
-
-<h4 id="Website">Website</h4>
+<h4>Webseite</h4>
 <p>Eine erste Anlaufstelle für Neugierige und Interessenten ist unsere Website <a href="http://www.weimarnetz.de">www.weimarnetz.de</a>. Die Inhalte sollten wir so aktuell wie möglich halten, um uns und andere nicht zu verwirren. Deshalb sind dort immer wieder Anpassungen erforderlich. Wir möchten uns auch regelmäßig mit unbedarfteren Nutzern treffen, um Schwachstellen in der Benutzerführung auf der Seite oder zu kompliziert aufgebaute Texte zu finden.</p>
 <p>Eng verbunden mit der Website ist unser Wiki unter <a href="https://wireless.subsignal.org/index.php?title=Hauptseite">wireless.subsignal.org</a>. Das Wiki verwenden wir, um bebilderte Anleitungen und Texte für Spezialisten abzulegen oder Dokumentationen zu schreiben, deren Inhalte immer wieder angepasst werden müssen. Diese Inhalte lassen sich leicht ändern und anpassen, bieten aber nicht die gleichen Designmöglichkeiten wie reine Websites. Außerdem neigen Wikis ständig zu einer gewissen Unordnung, die es schwer macht, schnell einen Überblick zu finden.</p>
 <p>Die Website pflegen wir in unserem Repository auf <a href="https://github.com/weimarnetz/web">https://github.com/weimarnetz/web</a>, und unter <a href="https://github.com/weimarnetz/wiki">https://github.com/weimarnetz/wiki</a> findet man Einstellungen und wichtige Dokumente unseres Wikis.</p>
@@ -108,10 +107,10 @@
 </ul>
 </p>
 
+
+<p id="Installation">&nbsp;</p>
 <hr>
-
-
-<h4 id="Installation">Installationen</h4>
+<h4>Installationen</h4>
 <p>Eine nachhaltige Verbesserung des Netzes können wir durch gezielte Installationen von Routern zwischen Gebäuden oder an markanten Punkten erreichen. Diese Orte zeichnen sich durch gerichtete Funkstrecken aus und können mehrere Himmelsrichtungen abdecken und sollten gut geplant und mit den Funknachbarn abgesprochen sein.</p>
 <p>Dir gefallen handfeste Ergebnisse und Höhenangst ist ein Fremdwort &mdash; dann bist du hier richtig.</p>
 
@@ -135,10 +134,10 @@
 </ul>
 </p>
 
+
+<p id="Firmware">&nbsp;</p>
 <hr>
-
-
-<h4 id="Firmware">Firmware</h4>
+<h4>Firmware</h4>
 <p>Das Herzstück für unser Netz und gleichzeitig einer unserer größten &quot;Brocken&quot; ist unsere Firmware, mit der wir unsere Geräte im Weimarnetz überhaupt zusammenbringen. Sie basiert auf OpenWRT und wir versuchen hier sehr stark auch Komponenten anderer Freifunk-Communities wiederzuverwenden. Neue Anforderungen, Weiterentwicklungen im OpenWRT oder durch neue, leistungsfähigere Hardware, die auf den Markt kommt erfordern ständige Anpassungen und vor allem regelmäßiges Testen. Die Firmwareentwicklung ist auch eng verzahnt mit den Nachbarprojekten Registrator und Meshkit. Änderungen an Firmware oder in der Administration können jeweils auch Auswirkungen aufeinander haben.</p>
 <p>Die Erweiterungen von OpenWRT für das Weimarnetz legen wir im Github unter <a href="https://github.com/weimarnetz/weimarnetz">https://github.com/weimarnetz/weimarnetz</a> und <a href="https://github.com/weimarnetz/openwrt-packages">https://github.com/weimarnetz/openwrt-packages</a> ab.</p>
 <p>Du sprichst fließend shell-Code, träumst in Lua oder spielst gern mit neuer Hardware und entwickelst gern nah an der Hardware, dann kann die Firmwareentwicklung ein spannendendes Feld für dich sein.</p>
@@ -165,10 +164,10 @@
 </ul>
 </p>
 
+
+<p id="Registrator">&nbsp;</p>
 <hr>
-
-
-<h4 id="Registrator">Registrator</h4>
+<h4>Registrator</h4>
 <p>Mit dem Registrator verwalten wir die sogenannten Knotennummern im Weimarnetz. Die Knotennummer ist eine eindeutig Nummer, die einen Router identifiziert und die Grundlage für seine Netzwerkkonfiguration bildet.</p>
 <p>Der Quellcode des Registrators ist unter <a href="https://github.com/weimarnetz/registrator">https://github.com/weimarnetz/registrator</a> zu finden.</p>
 <p>Mit node.js und Erfahrung im Betrieb von Diensten kannst du dich beim Registrator einbringen.</p>
@@ -192,11 +191,11 @@
 </ul>
 </p>
 
+
+<p id="Meshkit">&nbsp;</p>
 <hr>
-
-
-<h4 id="Meshkit">Meshkit</h4>
-<p>Das Meshkit gibt uns die Möglichkeit, individuelle und fertig konfigurierte Firmwareimages für Router auszuliefern. Mit dem Meshkit konnten wir die Komplexität und Anzahl der Schritte vom gekaufen Router zum einsatzbereiten Weimarnetzgerät extrem verringern und für Nutzer einen inzwischen recht einfachen Weg zur Teilnahme an Netz bereiten.</p>
+<h4>Meshkit</h4>
+<p>Das Meshkit gibt uns die Möglichkeit, individuelle und fertig konfigurierte Firmwareimages für Router auszuliefern. Mit dem Meshkit konnten wir die Komplexität und Anzahl der Schritte vom gekaufen Router zum einsatzbereiten Weimarnetzgerät extrem verringern und für Nutzer einen inzwischen recht einfachen Weg zur Teilnahme am Netz bereiten.</p>
 <p>Unsere angepasste Version des Meshkit kann man unter <a href="https://github.com/weimarnetz/meshkit">https://github.com/weimarnetz/meshkit</a> finden.</p>
 <p>Python ist für dich nicht nur eine Schlange? Dann passt du bestens zum Meshkit!</p>
 
@@ -222,10 +221,10 @@
 </ul>
 </p>
 
+
+<p id="Administration">&nbsp;</p>
 <hr>
-
-
-<h4 id="Administration">Administration</h4>
+<h4>Administration</h4>
 <p>Der Verein betreibt einen eigenen Rootserver, der in Berlin steht und betreibt darauf und bei befreundeten Communities insgesamt 3 virtuelle Server. Darauf laufen unter anderem diese Website, die VPN-Endpunkte für unsere Router, ein Jabber-Server und ein DynDNS-Dienst. Die Dienste und Betriebssysteme auf den Servern müssen regelmäßig aktuell gehalten und hin und wieder an neue Gegebenheiten angepasst werden. Ein Monitoringsystem informiert uns über Störungen an Hard- und Software.</p>
 <p>Für alle, die sich gern im Hintergrund aufhalten ist die Administration der Server ein idealer Job.</p>
 
