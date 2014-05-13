9b42abba71148f5c09f03a6415cc391a90dd9205
mt <mt@i3o.de>
2014-05-13 06:15:09 +0200

    fix xss

--- inc/activation.inc.php
+++ inc/activation.inc.php
@@ -9,13 +9,13 @@
       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><div class="container">';
       echo '<h2>Dein Internetzugang wurde erfolgreich freigeschaltet</h2>';
       if ($gwip <> '') {
-        echo '<p>Den Internetzugang stellt Dir <a href="http://', $gwip, '/cgi-bin/luci/freifunk/contact">dieser freundliche Nachbar</a> bereit.</p>';
+        echo '<p>Den Internetzugang stellt Dir <a href="http://', htmlspecialchars($gwip), '/cgi-bin/luci/freifunk/contact">dieser freundliche Nachbar</a> bereit.</p>';
       }  
       if ($neighborip <> '') {
-        echo '<p>Dein nächster Weimarnetzknoten wird von <a href="http://', $neighborip , '/cgi-bin/luci/freifunk/contact">diesem freundlichen Nachbarn</a> betrieben</p>';
+        echo '<p>Dein nächster Weimarnetzknoten wird von <a href="http://',htmlspecialchars($neighborip) , '/cgi-bin/luci/freifunk/contact">diesem freundlichen Nachbarn</a> betrieben</p>';
       }
       if ($origin <> '') {
-	echo '<p><small>PS: <a href="', urldecode($origin), '" target="_blank">Diese Seite</a> wolltest du ursprünglich ansehen.</small></p>';
+	echo '<p><small>PS: <a href="', htmlspecialchars(urldecode($origin)), '" target="_blank">Diese Seite</a> wolltest du ursprünglich ansehen.</small></p>';
       }
       echo "</div></div>";
     }
