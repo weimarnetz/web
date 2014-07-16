86720361515b1b51ed881e7e4cf17ec8da756ea7
andibraeu <ab@andi95.de>
2014-07-16 21:36:00 +0200

    services: added hint on internal addresses: working on #43

--- services/index.php
+++ services/index.php
@@ -12,7 +12,8 @@
     
     <!-- CONTENT -->
     <h1>Dienste im Weimarnetz</h1>
-    Diese Dienste werden von Teilnehmern im Weimarnetz angeboten. Die Übersicht wird automatisiert erstellt und regelmäßig aktualisiert.</br>
+		Diese Dienste werden von Teilnehmern im Weimarnetz angeboten. Die Übersicht wird automatisiert erstellt und regelmäßig aktualisiert.</br>
+		<div class="row alert alert-warning">Hier aufgelistete Dienste sind in der Regel nur innerhalb des Weimarnetzes oder aus anderen Freifunk-Communities, die über das <a href="http://freifunk.net/blog/2014/02/das-intercity-vpn/" target="_blank">InterCity-VPN</a> mit uns verbinden sind, erreichbar. Deshalb funktionieren einige der Links nicht, wenn man sich aus dem Internet verbindet.</div>
     <?php include("../inc/olsr_services.inc.php") ?>
     
     <?php include("../inc/footer.inc.php") ?>
