da6ce5bee45d729c83b0282c3febc25ce297e0ce
andibraeu <ab@andi95.de>
2015-04-03 17:03:49 +0200

    newsfeed: removed unnecessary stuff

--- inc/allnews.inc.php
+++ inc/allnews.inc.php
@@ -19,14 +19,7 @@ $feed->handle_content_type();
 	/*
 	Here, we'll loop through all of the items in the feed, and $item represents the current item in the loop.
 	*/
-	$counter=1;
 	foreach ($feed->get_items(0, 5) as $item):
-//	if ($item->get_title() == "") {
-//		continue;
-//	}
-//	if ($counter > 5) {
-//		break;
-//	}
 	?>
  
 		<li class="item">
@@ -35,7 +28,6 @@ $feed->handle_content_type();
 		</li>
  
 	<?php 
-	$counter++;
 	endforeach; ?>
 </ul>
 
